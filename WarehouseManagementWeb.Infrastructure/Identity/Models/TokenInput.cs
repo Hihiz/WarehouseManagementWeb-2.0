8c173cc6210211a6fc76f3c2c58a8e566ee5ef24
Hihiz <public@mail.ru>
2026-02-27 23:35:54 +0300

    Добавить входную модель токенов

---
+++ WarehouseManagementWeb.Infrastructure/Identity/Models/TokenInput.cs
@@ -0,0 +1,18 @@
+﻿namespace WarehouseManagementWeb.Infrastructure.Identity.Models
+{
+    /// <summary>
+    /// Класс входной модели токенов пользователя.
+    /// </summary>
+    public class TokenInput
+    {
+        /// <summary>
+        /// Токен доступа пользователя.
+        /// </summary>
+        public string? AccessToken { get; set; }
+
+        /// <summary>
+        /// Токен обновления пользователя.
+        /// </summary>
+        public string? RefreshToken { get; set; }
+    }
+}
