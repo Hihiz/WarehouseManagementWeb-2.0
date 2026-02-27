8bb997953b9241c9e4b79bd775ba3678572622d1
Hihiz <public@mail.ru>
2026-02-27 23:35:10 +0300

    Добавить интерфейс сервиса аутентификации пользователей

---
+++ WarehouseManagementWeb.Infrastructure/Interfaces/IAuthService.cs
@@ -0,0 +1,31 @@
+﻿
+using WarehouseManagementWeb.Infrastructure.Identity.Models;
+
+namespace WarehouseManagementWeb.Infrastructure.Interfaces
+{
+    /// <summary>
+    /// Интерфейс сервиса аутентификации пользователей.
+    /// </summary>
+    public interface IAuthService
+    {
+        /// <summary>
+        /// Метод регистрирует нового пользователя.
+        /// </summary>
+        /// <param name="userSignUpInput">Входная модель.</param>
+        /// <returns>Выходная модель.</returns>
+        Task<UserSignUpOutput> SignUpAsync(UserSignUpInput userSignUpInput);
+
+        /// <summary>
+        /// Метод аутентифицирует пользователя.
+        /// </summary>
+        /// <param name="userSignInInput">Входная модель.</param>
+        /// <returns>Выходная модель.</returns>
+        Task<UserSignInOutput> SignInAsync(UserSignInInput userSignInInput);
+
+        /// <summary>
+        /// Метод выходит из аккаунта пользователя.
+        /// </summary>
+        /// <param name="userEmail">Email пользователя.</param>
+        Task LogoutAsync(string userEmail);
+    }
+}
