6933b81aecf3e934bc1f92e9606f96faf3f22206
Hihiz <public@mail.ru>
2026-02-27 23:35:33 +0300

    Добавить интерфейс сервиса токенов

---
+++ WarehouseManagementWeb.Infrastructure/Interfaces/ITokenService.cs
@@ -0,0 +1,23 @@
+﻿using WarehouseManagementWeb.Infrastructure.Identity.Models;
+
+namespace WarehouseManagementWeb.Infrastructure.Interfaces
+{
+    /// <summary>
+    /// Интерфейс сервиса токенов пользователей.
+    /// </summary>
+    public interface ITokenService
+    {
+        /// <summary>
+        /// Метод обновляет токены пользователя (accessToken и refreshToken).
+        /// </summary>
+        /// <param name="tokenInput">Входная модель.</param>
+        /// <returns>Выходная модель.</returns>
+        Task<TokenOutput> RefreshTokenAsync(TokenInput tokenInput);
+
+        /// <summary>
+        /// Метод сбрасывает refreshToken пользователя по Email.
+        /// </summary>
+        /// <param name="userEmail">Email пользователя.</param>
+        Task RevokeRefreshTokenUserByEmailAsync(string userEmail);    
+    }
+}
