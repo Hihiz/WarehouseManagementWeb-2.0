0cb521be8e580afd9f669363f7fb790816990c05
Hihiz <public@mail.ru>
2026-02-27 23:36:19 +0300

    Добавить входную модель аутентификации

---
+++ WarehouseManagementWeb.Infrastructure/Identity/Models/UserSignInInput.cs
@@ -0,0 +1,18 @@
+﻿namespace WarehouseManagementWeb.Infrastructure.Identity.Models
+{
+    /// <summary>
+    /// Класс входной модели прохождения аутентификации пользователя.
+    /// </summary>
+    public class UserSignInInput
+    {
+        /// <summary>
+        /// Логин пользователя.
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя.
+        /// </summary>
+        public string? Password { get; set; }
+    }
+}
