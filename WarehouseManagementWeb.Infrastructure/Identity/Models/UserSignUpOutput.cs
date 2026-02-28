bbf753515a2ab07d9c02dd47d3070a5d371a3adc
Hihiz <public@mail.ru>
2026-02-28 19:28:24 +0300

    Добавить выходную модель регистрации

---
+++ WarehouseManagementWeb.Infrastructure/Identity/Models/UserSignUpOutput.cs
@@ -0,0 +1,23 @@
+﻿namespace WarehouseManagementWeb.Infrastructure.Identity.Models
+{
+    /// <summary>
+    /// Класс выходной модели регистрации пользователя.
+    /// </summary>
+    public class UserSignUpOutput
+    {
+        /// <summary>
+        /// Имя пользователя.
+        /// </summary>
+        public string? FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия пользователя.
+        /// </summary>
+        public string? LastName { get; set; }
+
+        /// <summary>
+        /// Email пользователя.
+        /// </summary>
+        public string? Email { get; set; }       
+    }
+}
