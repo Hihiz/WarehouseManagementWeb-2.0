54ea273839c85ed0f855e5a9a24fe1f8981cbd02
Hihiz <public@mail.ru>
2026-02-28 19:46:20 +0300

    Реализовать метод обновления токенов

--- WarehouseManagementWeb.Infrastructure/Services/TokenService.cs
+++ WarehouseManagementWeb.Infrastructure/Services/TokenService.cs
@@ -1,4 +1,10 @@
-﻿using WarehouseManagementWeb.Infrastructure.Identity.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using WarehouseManagementWeb.Infrastructure.Helpers;
+using WarehouseManagementWeb.Infrastructure.Identity;
+using WarehouseManagementWeb.Infrastructure.Identity.Models;
 using WarehouseManagementWeb.Infrastructure.Interfaces;
 
 namespace WarehouseManagementWeb.Infrastructure.Services
@@ -8,10 +14,83 @@ namespace WarehouseManagementWeb.Infrastructure.Services
     /// </summary>
     public class TokenService : ITokenService
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<TokenService> _logger;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="userManager">Менеджер пользователей.</param>
+        /// <param name="configuration">Конфигурация.</param>
+        /// <param name="logger">Логгер.</param>
+        public TokenService(UserManager<ApplicationUser> userManager,
+            IConfiguration configuration,
+            ILogger<TokenService> logger)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
         /// <inheritdoc />
         public async Task<TokenOutput> RefreshTokenAsync(TokenInput tokenInput)
         {
-           
+            try
+            {
+                if (tokenInput is null)
+                {
+                    throw new InvalidOperationException("Недопустимый токен доступа или токен обновления.");
+                }
+
+                ClaimsPrincipal claimsPrincipal = _configuration.GetPrincipalFromExpiredToken(tokenInput.AccessToken);
+
+                if (claimsPrincipal is null)
+                {
+                    throw new InvalidOperationException("Недопустимый токен доступа или токен обновления.");
+                }
+
+                string userName = claimsPrincipal.Identity!.Name!;
+
+                // Получаем пользователя по userName.
+                ApplicationUser? user = await _userManager.FindByEmailAsync(userName);
+
+                if (user is null || user.RefreshToken != tokenInput.RefreshToken
+                    || user.RefreshTokenExpiryTime <= DateTime.UtcNow)
+                {
+                    throw new InvalidOperationException($"Недопустимый токен доступа или токен обновления.");
+                }
+
+                // Получаем новый токен доступа.
+                string newAccessToken = _configuration.GenerateAccessToken(claimsPrincipal.Claims.ToList());
+
+                // Получаем новый токен обновления.
+                string newRefreshToken = _configuration.GenerateRefreshToken();
+
+                // Обновляем токен обновления пользователю.
+                user.RefreshToken = newRefreshToken;
+
+                // Обновляем время жизни токена обновления.
+                user.RefreshTokenExpiryTime = DateTime.UtcNow.AddMinutes(_configuration.GetSection(
+                    "Jwt:RefreshTokenValidityInMinutes").Get<int>());
+
+                await _userManager.UpdateAsync(user);
+
+                TokenOutput result = new TokenOutput
+                {
+                    AccessToken = newAccessToken,
+                    RefreshToken = newRefreshToken
+                };
+
+                return result;
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                throw;
+            }
         }
 
         /// <inheritdoc />
