2930eed659636c4346f5a4a064aa9a23c0dd8bc3
Hihiz <public@mail.ru>
2026-02-28 19:41:20 +0300

    Добавить класс сервиса аутентификации

---
+++ WarehouseManagementWeb.Infrastructure/Services/AuthService.cs
@@ -0,0 +1,29 @@
+﻿using WarehouseManagementWeb.Infrastructure.Identity.Models;
+using WarehouseManagementWeb.Infrastructure.Interfaces;
+
+namespace WarehouseManagementWeb.Infrastructure.Services
+{
+    /// <summary>
+    /// Класс реализует методы сервиса аутентификации пользователей.
+    /// </summary>
+    public class AuthService : IAuthService
+    {
+        /// <inheritdoc />
+        public async Task<UserSignUpOutput> SignUpAsync(UserSignUpInput userSignUpInput)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public async Task<UserSignInOutput> SignInAsync(UserSignInInput userSignInInput)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public async Task LogoutAsync(string userEmail)
+        {
+           
+        }
+    }
+}
