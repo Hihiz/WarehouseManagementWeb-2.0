cd773f80c6c1180058d665a1e94e1e43750f6967
Hihiz <public@mail.ru>
2026-02-28 19:45:46 +0300

    Добавить класс сервиса токенов

---
+++ WarehouseManagementWeb.Infrastructure/Services/TokenService.cs
@@ -0,0 +1,23 @@
+﻿using WarehouseManagementWeb.Infrastructure.Identity.Models;
+using WarehouseManagementWeb.Infrastructure.Interfaces;
+
+namespace WarehouseManagementWeb.Infrastructure.Services
+{
+    /// <summary>
+    /// Класс реализует методы сервиса токенов пользователей.
+    /// </summary>
+    public class TokenService : ITokenService
+    {
+        /// <inheritdoc />
+        public async Task<TokenOutput> RefreshTokenAsync(TokenInput tokenInput)
+        {
+           
+        }
+
+        /// <inheritdoc />
+        public async Task RevokeRefreshTokenUserByEmailAsync(string userEmail)
+        {
+           
+        }
+    }
+}
