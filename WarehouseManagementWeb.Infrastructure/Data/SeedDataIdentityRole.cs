b6658d3c2c46ba3ccf856eae6dc611fd90bc563a
Hihiz <public@mail.ru>
2026-03-01 04:44:05 +0300

    Добавить начальную инициализацию

---
+++ WarehouseManagementWeb.Infrastructure/Data/SeedDataIdentityRole.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using WarehouseManagementWeb.Infrastructure.Identity;
+
+namespace WarehouseManagementWeb.Infrastructure.Data
+{
+    /// <summary>
+    /// Класс инициализирует таблицу ролей начальными данными.
+    /// </summary>
+    public class SeedDataIdentityRole
+    {
+        /// <summary>
+        /// Метод проверяет существование и создает роли.
+        /// </summary>
+        /// <param name="serviceProvider">Провайдер сервисов.</param>
+        public static async Task SeedRoleAsync(IServiceProvider serviceProvider)
+        {
+            RoleManager<IdentityRole<long>> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<
+                long>>>();
+
+            UserManager<ApplicationUser> userManager = serviceProvider.GetRequiredService<UserManager<
+                ApplicationUser>>();
+
+            string[] roles = ["User", "Admin"];
+
+            foreach (var role in roles)
+            {
+                bool isRoleExist = await roleManager.RoleExistsAsync(role);
+
+                if (!isRoleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole<long>
+                    {
+                        Name = role,
+                        NormalizedName = role.ToUpper()
+                    });
+                }
+            }
+
+            // Создание администратора по умолчанию.
+            string adminEmail = "admin@example.com";
+
+            ApplicationUser? adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser is null)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    FirstName = "AdminFirstName",
+                    LastName = "AdminLastName",
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true
+                };
+
+                //string? adminPassword = "Admin123!";
+                string? adminPassword = "1";
+
+                await userManager.CreateAsync(user, adminPassword);
+
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+    }
+}
