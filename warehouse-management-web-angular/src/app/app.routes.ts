366b19f91931cae77996c597f8b2aa7a72b79e13
Hihiz <public@mail.ru>
2026-03-01 04:57:28 +0300

    Добавить начальные файлы

--- warehouse-management-web-angular/src/app/app.routes.ts
+++ warehouse-management-web-angular/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/components/signup.component';
+import { SigninComponent } from './auth/signin/components/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Роуты для Auth.
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+
+  {
+    path: 'signin',
+    component: SigninComponent,
+  },
+
+  {
+    path: '',
+    redirectTo: '/signin',
+    pathMatch: 'full',
+  },
+];
---
+++ warehouse-management-web-angular/src/app/core/interceptors/http-request-interceptor.ts
@@ -0,0 +1,63 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, switchMap, throwError } from 'rxjs';
+import { AuthService } from '../../auth/services/auth.service';
+
+/**
+ * Функция перехватыает HTTP запросы.
+ */
+export const httpRequestInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+
+  const accessToken = localStorage.getItem('utoken');
+
+  console.log('accessToken', accessToken);
+
+  if (accessToken) {
+    req = req.clone({
+      setHeaders: { Authorization: `Bearer ${accessToken}` },
+    });
+  }
+
+  let responseNext = next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (req.url.includes('refresh-token')) {
+        return throwError(() => error);
+      }
+      
+      if (error.status !== 401) {
+        return throwError(() => error);
+      }
+
+      return authService.refreshToken().pipe(
+        switchMap((response: any) => {
+          const newAccessToken = response.accessToken;
+          const newRefreshToken = response.refreshToken;
+
+          localStorage.setItem('utoken', newAccessToken);
+          localStorage.setItem('urefresh-token', newRefreshToken);
+
+          // Обновляем токены текущему пользователю.
+          const currentUser = authService.userSignIn$.value;
+          if (currentUser) {
+            currentUser.accessToken = newAccessToken;
+            currentUser.refreshToken = newRefreshToken;
+            authService.userSignIn$.next(currentUser);
+          }
+
+          return next(
+            req.clone({
+              setHeaders: { Authorization: `Bearer ${newAccessToken}` },
+            }),
+          );
+        }),
+
+        catchError((err) => {
+          return throwError(() => err);
+        }),
+      );
+    }),
+  );
+
+  return responseNext;
+};
