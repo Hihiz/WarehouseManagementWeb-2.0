dd40010a75c7629693d5eb8e86c14da02b5ea69d
Hihiz <public@mail.ru>
2026-03-01 04:48:51 +0300

    Зарегистрировать зависимости

--- WarehouseManagementWeb.Api/Program.cs
+++ WarehouseManagementWeb.Api/Program.cs
@@ -1,10 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WarehouseManagementWeb.Api.Middlewares;
+using WarehouseManagementWeb.Infrastructure;
+using WarehouseManagementWeb.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ApiCorsPolicy",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+        });
+});
+
+// Добавляем роботу токена в Swagger.
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WarehouseManagementWeb",
+        Version = "v1"
+    });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Введите JWT токен",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                          new string[] { }
+                    }
+                });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
@@ -12,10 +65,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    IServiceProvider serviceProvider = scope.ServiceProvider;
+
+    ApplicationDbContext db = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+    await db.Database.MigrateAsync();
+
+    await SeedDataIdentityRole.SeedRoleAsync(serviceProvider);
+}
+
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseCors("ApiCorsPolicy")
+    .UseAuthentication()
+    .UseAuthorization();
 
 app.MapControllers();
 
