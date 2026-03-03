f29f273c20e7d91541ad89255b45519cf969e818
Hihiz <public@mail.ru>
2026-03-03 23:40:58 +0300

    Добавить сущность клиента

---
+++ WarehouseManagementWeb.Domain/Entities/ClientEntity.cs
@@ -0,0 +1,30 @@
+﻿using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Domain.Entities
+{
+    /// <summary>
+    /// Класс клиента сопоставляется с таблицей directory.Clients.
+    /// </summary>
+    public class ClientEntity
+    {
+        /// <summary>
+        /// PK.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование клиента.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Адрес клиента.
+        /// </summary>
+        public string? Address { get; set; }
+
+        /// <summary>
+        /// Статус клиента в значении перечисления.
+        /// </summary>
+        public DirectoryStatusEnum ClientStatusEnum { get; set; }
+    }
+}
