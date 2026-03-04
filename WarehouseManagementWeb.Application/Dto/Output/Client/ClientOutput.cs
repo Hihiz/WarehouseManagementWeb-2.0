8dd3987cfcc8f641e2674246acbf91286ac753d2
Hihiz <public@mail.ru>
2026-03-05 02:36:59 +0300

    Добавить выходную модель клиентов

---
+++ WarehouseManagementWeb.Application/Dto/Output/Client/ClientOutput.cs
@@ -0,0 +1,31 @@
+﻿using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Application.Dto.Output.Client
+{
+    /// <summary>
+    /// Класс выходной модели клиентов.
+    /// </summary>
+
+    public class ClientOutput
+    {
+        /// <summary>
+        /// Id клиента.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование клиента.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Адрес клиента.
+        /// </summary>
+        public string? Address { get; set; }
+
+        /// <summary>
+        /// Статус клиента в значении перечисления.
+        /// </summary>
+        public DirectoryStatusEnum ClientStatusEnum { get; set; }
+    }
+}
