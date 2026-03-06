e5e07c462e2801f6a56792139f48a44f5399a53e
Hihiz <public@mail.ru>
2026-03-06 23:18:30 +0300

    Добавить метод получения всех клиентов

--- WarehouseManagementWeb.Application/Interfaces/Repositories/Client/IClientRepository.cs
+++ WarehouseManagementWeb.Application/Interfaces/Repositories/Client/IClientRepository.cs
@@ -1,10 +1,16 @@
-﻿namespace WarehouseManagementWeb.Application.Interfaces.Repositories.Client
+﻿using WarehouseManagementWeb.Application.Dto.Output.Client;
+
+namespace WarehouseManagementWeb.Application.Interfaces.Repositories.Client
 {
     /// <summary>
     /// Интерфейс репозитория клиента.
     /// </summary>
     public interface IClientRepository
     {
-
+        /// <summary>
+        /// Метод получает список клиентов.
+        /// </summary>
+        /// <returns>Список клиентов.</returns>
+        Task<IEnumerable<ClientOutput>> GetClientsAsync();
     }
 }
