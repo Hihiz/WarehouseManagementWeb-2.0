72d9d065eb690603b8b7592e08a02227c6b2ff73
Hihiz <public@mail.ru>
2026-03-06 23:17:34 +0300

    Добавить интерфейс репозитория клиентов

---
+++ WarehouseManagementWeb.Application/Interfaces/Repositories/Client/IClientRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace WarehouseManagementWeb.Application.Interfaces.Repositories.Client
+{
+    /// <summary>
+    /// Интерфейс репозитория клиента.
+    /// </summary>
+    public interface IClientRepository
+    {
+
+    }
+}
