0c8740b06638d654a322a712924eb359bf90b82e
Hihiz <public@mail.ru>
2026-03-08 03:57:23 +0300

    Добавить входную модель редактирования клиента

---
+++ WarehouseManagementWeb.Application/Dto/Input/Client/UpdateClientInput.cs
@@ -0,0 +1,23 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.Client
+{
+    /// <summary>
+    /// Класс входной модели редактирования клиента.
+    /// </summary>
+    public class UpdateClientInput
+    {
+        /// <summary>
+        /// Id клиента.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование клиента.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Адрес клиента.
+        /// </summary>
+        public string? Address { get; set; }
+    }
+}
