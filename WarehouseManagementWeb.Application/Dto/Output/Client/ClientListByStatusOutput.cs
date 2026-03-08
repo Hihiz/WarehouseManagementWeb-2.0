4c8505047bc6c758448fefcf1f6478d0ddf51f09
Hihiz <public@mail.ru>
2026-03-08 03:58:40 +0300

    Добавить выходную модель разделения клиентов по статусам

---
+++ WarehouseManagementWeb.Application/Dto/Output/Client/ClientListByStatusOutput.cs
@@ -0,0 +1,18 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Output.Client
+{
+    /// <summary>
+    /// Класс выходной модели клиентов разделенных по статусам.
+    /// </summary>
+    public class ClientListByStatusOutput
+    {
+        /// <summary>
+        /// Список активных клиентов.
+        /// </summary>
+        public IEnumerable<ClientOutput>? ActiveClients { get; set; }
+
+        /// <summary>
+        /// Список клиентов находящихся в архиве.
+        /// </summary>
+        public IEnumerable<ClientOutput>? ArchivedClients { get; set; }
+    }
+}
