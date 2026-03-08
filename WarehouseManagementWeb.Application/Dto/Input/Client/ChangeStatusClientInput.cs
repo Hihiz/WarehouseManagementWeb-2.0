b419ddb1b8a5bf3efe8fb9dcc62f1454024b3edb
Hihiz <public@mail.ru>
2026-03-08 03:57:01 +0300

    Добавить входную модель обновления статуса клиента

---
+++ WarehouseManagementWeb.Application/Dto/Input/Client/ChangeStatusClientInput.cs
@@ -0,0 +1,19 @@
+﻿using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Application.Dto.Input.Client
+{
+    /// <summary>
+    /// Класс входной модели обновления статуса клиента.
+    /// </summary>
+    public class ChangeStatusClientInput
+    {
+        /// Id клиента.
+        /// </summary>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Статус клиента.
+        /// </summary>
+        public DirectoryStatusEnum ClientStatusEnum { get; set; }
+    }
+}
