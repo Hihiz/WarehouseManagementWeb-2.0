8a9ea2ed44b279cc2093f6b2ba63577c29c8e866
Hihiz <public@mail.ru>
2026-03-09 21:06:58 +0300

    Добавить тест удаления

---
+++ WarehouseManagementWeb.Tests/BaseIntegrationTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+using WarehouseManagementWeb.Infrastructure.Data;
+using WarehouseManagementWeb.Infrastructure.Repositories;
+
+namespace WarehouseManagementWeb.Tests
+{
+    /// <summary>
+    /// Базовый класс интеграционных тестов.
+    /// </summary>
+    public class BaseIntegrationTest : IDisposable
+    {
+        private readonly IConfiguration appConfiguration;
+        private readonly IDbContextTransaction _transaction;
+
+        protected internal readonly ClientRepository clientRepository;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public BaseIntegrationTest()
+        {
+            var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json");
+
+            appConfiguration = builder.Build();
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseNpgsql(appConfiguration.GetConnectionString("DefaultConnection"));
+            var applicationDbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            clientRepository = new ClientRepository(applicationDbContext);
+
+            _transaction = applicationDbContext.Database.BeginTransaction(System.Data.IsolationLevel.ReadCommitted);
+        }
+
+        public void Dispose()
+        {
+            // Откатываем изменения после тест метода.
+            _transaction.Rollback();
+            _transaction.Dispose();
+        }
+    }
+}
