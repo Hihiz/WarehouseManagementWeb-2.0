7cd8545ada9ae469ba568db82b018362a20eebe1
Hihiz <public@mail.ru>
2026-03-09 18:32:28 +0300

    Добавить интерфейс сервиса клиентов

---
+++ WarehouseManagementWeb.Application/Interfaces/Services/Client/IClientService.cs
@@ -0,0 +1,14 @@
+﻿using WarehouseManagementWeb.Application.Dto.Input.Client;
+using WarehouseManagementWeb.Application.Dto.Output.Client;
+using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Application.Interfaces.Services.Client
+{
+    /// <summary>
+    /// Интерфейс сервиса клиентов.
+    /// </summary>
+    public interface IClientService
+    {
+       
+    }
+}
