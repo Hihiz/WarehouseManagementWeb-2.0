c644c0be1b0b05e51ceedbd310ae5e9d36e23eb1
Hihiz <public@mail.ru>
2026-03-09 21:03:21 +0300

    Добавить тест получения клиентов

---
+++ WarehouseManagementWeb.Tests/Integration/Repository/Client/GetClientsTest.cs
@@ -0,0 +1,13 @@
+﻿namespace WarehouseManagementWeb.Tests.Integration.Repository.Client
+{
+    public class GetClientsTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetClientsAsyncTest()
+        {
+            var result = await clientRepository.GetClientsAsync();
+
+            Assert.NotNull(result);
+        }
+    }
+}
