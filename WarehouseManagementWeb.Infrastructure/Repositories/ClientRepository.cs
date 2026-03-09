e9542e0fd6d2289a0672fbe98c4578eba267cae8
Hihiz <public@mail.ru>
2026-03-09 18:42:18 +0300

    Добавить класс репозитория клиентов

---
+++ WarehouseManagementWeb.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,70 @@
+﻿using WarehouseManagementWeb.Application.Dto.Output.Client;
+using WarehouseManagementWeb.Application.Interfaces.Repositories.Client;
+using WarehouseManagementWeb.Domain.Entities;
+using WarehouseManagementWeb.Domain.Enums;
+using WarehouseManagementWeb.Infrastructure.Data;
+
+namespace WarehouseManagementWeb.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Класс реализует методы репозитория клиентов.
+    /// </summary>
+    public class ClientRepository : IClientRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="db">Класс контекста.</param>
+        public ClientRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Task ChangeStatusClientAsync(int clientId, DirectoryStatusEnum statusEnum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CheckClientExistsByNameAndIdAsync(int clientId, string clientName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CheckClientExistsByNameAsync(string clientName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateClientAsync(ClientEntity clientEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ClientOutput>> GetActiveClientsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ClientOutput?> GetClientByIdAsync(int clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ClientOutput>> GetClientsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveClientAsync(int clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateClientAsync(ClientEntity clientEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
