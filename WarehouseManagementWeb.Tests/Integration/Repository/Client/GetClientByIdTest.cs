df57b95ce3e18d1996ae7a9d49e30c003daaf1c4
Hihiz <public@mail.ru>
2026-03-09 21:03:31 +0300

    Добавить тест получения деталей клиента

---
+++ WarehouseManagementWeb.Tests/Integration/Repository/Client/GetClientByIdTest.cs
@@ -0,0 +1,13 @@
+﻿namespace WarehouseManagementWeb.Tests.Integration.Repository.Client
+{
+    public class GetClientByIdTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetClientByIdAsyncTest()
+        {
+            var result = await clientRepository.GetClientByIdAsync(1);
+
+            Assert.NotNull(result);
+        }
+    }
+}
