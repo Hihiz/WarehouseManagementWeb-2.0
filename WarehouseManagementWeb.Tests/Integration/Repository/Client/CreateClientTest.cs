8726b3b69683f1170f021ed5da2e4245ae355f85
Hihiz <public@mail.ru>
2026-03-09 21:04:39 +0300

    Добавить тест создания клиента

---
+++ WarehouseManagementWeb.Tests/Integration/Repository/Client/CreateClientTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseManagementWeb.Domain.Entities;
+
+namespace WarehouseManagementWeb.Tests.Integration.Repository.Client
+{
+    public class CreateClientTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task CreateClientAsyncTest()
+        {
+            // Arrange
+            var name = Guid.NewGuid().ToString();
+
+            var client = new ClientEntity
+            {
+                Name = name,
+                Address = "Test Address",
+            };
+
+            // Act & Assert
+            await clientRepository.CreateClientAsync(client);
+        }
+
+        [Fact]
+        public async Task CreateClientAsyncDuplicateNameTest()
+        {
+            // Arrange
+            var name = Guid.NewGuid().ToString();
+
+            var client1 = new ClientEntity
+            {
+                Name = name,
+                Address = "Test Address1",
+            };
+
+            await clientRepository.CreateClientAsync(client1);
+
+            var client2 = new ClientEntity
+            {
+                Name = name,
+                Address = "Test Address2",
+            };
+
+            // Act & Assert
+            await Assert.ThrowsAsync<DbUpdateException>(
+                    async () => await clientRepository.CreateClientAsync(client2));
+        }
+    }
+}
