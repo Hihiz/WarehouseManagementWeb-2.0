0a9fa507b64686953107c06a6931afd00e4ad3e2
Hihiz <public@mail.ru>
2026-03-09 18:32:59 +0300

    Добавить методы

--- WarehouseManagementWeb.Application/Interfaces/Services/Client/IClientService.cs
+++ WarehouseManagementWeb.Application/Interfaces/Services/Client/IClientService.cs
@@ -9,6 +9,48 @@ namespace WarehouseManagementWeb.Application.Interfaces.Services.Client
     /// </summary>
     public interface IClientService
     {
-       
+        /// <summary>
+        /// Метод получает список клиентов.
+        /// </summary>
+        /// <returns>Список клиентов.</returns>
+        Task<ClientListByStatusOutput> GetClientsAsync();
+
+        /// <summary>
+        /// Метод получает список активных клиентов.
+        /// </summary>
+        /// <returns>Список активных клиентов.</returns>
+        Task<IEnumerable<ClientOutput>> GetActiveClientsAsync();
+
+        /// <summary>
+        /// Метод получает клиента по Id.
+        /// </summary>
+        /// <param name="clientId">Id клиента.</param>
+        /// <returns>Данные клиента.</returns>
+        Task<ClientOutput?> GetClientByIdAsync(int clientId);
+
+        /// <summary>
+        /// Метод добавляет клиента.
+        /// </summary>
+        /// <param name="createClientInput">Входная модель.</param>
+        Task CreateClientAsync(CreateClientInput createClientInput);
+
+        /// <summary>
+        /// Метод редактирует клиента.
+        /// </summary>
+        /// <param name="updateClientInput">Входная модель.</param>
+        Task UpdateClientAsync(UpdateClientInput updateClientInput);
+
+        /// <summary>
+        /// Метод обновляет статус клиенту.
+        /// </summary>
+        /// <param name="clientId">Id клиента.</param>
+        /// <param name="statusEnum">Новый статус клиента.</param>
+        Task ChangeStatusClientAsync(int clientId, DirectoryStatusEnum statusEnum);
+
+        /// <summary>
+        /// Метод удаляет клиента.
+        /// </summary>
+        /// <param name="clientId">Id клиента.</param>
+        Task RemoveClientAsync(int clientId);
     }
 }
