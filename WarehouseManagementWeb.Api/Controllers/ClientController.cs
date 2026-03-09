e54fec08c8c5b6fe452bd6d5fce7fea5d47011dc
Hihiz <public@mail.ru>
2026-03-09 18:27:08 +0300

    Добавить контроллер клиентов

---
+++ WarehouseManagementWeb.Api/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseManagementWeb.Application.Interfaces.Services.Client;
+
+namespace WarehouseManagementWeb.Api.Controllers
+{
+    /// <summary>
+    /// Контроллер клиентов.
+    /// </summary>
+    [Authorize]
+    [Route("api/directory/client")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientService _clientService;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="clientService">Сервис клиентов.</param>
+        public ClientController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+    }
+}
