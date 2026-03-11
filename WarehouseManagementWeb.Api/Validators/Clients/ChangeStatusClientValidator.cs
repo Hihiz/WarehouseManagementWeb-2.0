ea4989ceb50bc89a759e2e095870f69682cb634f
Hihiz <public@mail.ru>
2026-03-11 15:56:14 +0300

    Добавить валидатор обновления статуса клиента

---
+++ WarehouseManagementWeb.Api/Validators/Clients/ChangeStatusClientValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using WarehouseManagementWeb.Api.Validators.Constants;
+using WarehouseManagementWeb.Application.Dto.Input.Client;
+using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Api.Validators.Clients
+{
+    /// <summary>
+    /// Класс валидатора обновления статуса клиента.
+    /// </summary>
+    public class ChangeStatusClientValidator : AbstractValidator<ChangeStatusClientInput>
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public ChangeStatusClientValidator()
+        {
+            RuleFor(c => c.ClientId)
+               .Must(c => c > 0).WithMessage(ValidationConst.NOT_VALID_CLIENT_ID);
+
+            RuleFor(c => c.ClientStatusEnum)
+                .IsInEnum()
+                .NotEqual(DirectoryStatusEnum.Undefined).WithMessage(ValidationConst.NOT_VALID_CLIENT_STATUS);
+        }
+    }
+}
