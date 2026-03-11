ca98941edaf1bbe53fbdf01691551bcbe4105951
Hihiz <public@mail.ru>
2026-03-11 15:56:26 +0300

    Добавить валидатор создания клиента

---
+++ WarehouseManagementWeb.Api/Validators/Clients/CreateClientValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using WarehouseManagementWeb.Api.Validators.Constants;
+using WarehouseManagementWeb.Application.Dto.Input.Client;
+
+namespace WarehouseManagementWeb.Api.Validators.Clients
+{
+    /// <summary>
+    /// Класс валидатора создания клиента.
+    /// </summary>
+    public class CreateClientValidator : AbstractValidator<CreateClientInput>
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public CreateClientValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotNull().WithMessage(ValidationConst.NOT_VALID_CLIENT_NAME)
+                .NotEmpty().WithMessage(ValidationConst.EMPTY_CLIENT_NAME)
+                .MinimumLength(3).WithMessage(ValidationConst.MINIMUM_LENGTH_CLIENT_NAME)
+                .MaximumLength(20).WithMessage(ValidationConst.MAXIMUM_LENGTH_CLIENT_NAME);
+
+            RuleFor(c => c.Address)
+                .NotNull().WithMessage(ValidationConst.NOT_VALID_CLIENT_ADDRESS)
+                .NotEmpty().WithMessage(ValidationConst.EMPTY_CLIENT_ADDRESS)
+                .MinimumLength(3).WithMessage(ValidationConst.MINIMUM_LENGTH_CLIENT_ADDRESS)
+                .MaximumLength(20).WithMessage(ValidationConst.MAXIMUM_LENGTH_CLIENT_ADDRESS);
+        }
+    }
+}
