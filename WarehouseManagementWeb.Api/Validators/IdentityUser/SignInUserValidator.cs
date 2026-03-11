127d9978ab312b1d7fc8aa064b6eaddf2ded3c2e
Hihiz <public@mail.ru>
2026-03-11 15:57:54 +0300

    Добавить валидатор аутентификации пользователя

---
+++ WarehouseManagementWeb.Api/Validators/IdentityUser/SignInUserValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using WarehouseManagementWeb.Api.Validators.Constants;
+using WarehouseManagementWeb.Infrastructure.Identity.Models;
+
+namespace WarehouseManagementWeb.Api.Validators.IdentityUser
+{
+    /// <summary>
+    /// Класс валидатора аутентификации пользователя.
+    /// </summary>
+    public class SignInUserValidator : AbstractValidator<UserSignInInput>
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public SignInUserValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotNull().WithMessage(ValidationConst.NOT_VALID_USER_EMAIL)
+                .NotEmpty().WithMessage(ValidationConst.EMPTY_USER_EMAIL)
+                .EmailAddress().WithMessage(ValidationConst.INVALID_USER_EMAIL_FORMAT);
+
+            RuleFor(x => x.Password)
+               .NotNull().WithMessage(ValidationConst.NOT_VALID_USER_PASSWORD)
+               .NotEmpty().WithMessage(ValidationConst.EMPTY_USER_PASSWORD);
+
+        }
+    }
+}
