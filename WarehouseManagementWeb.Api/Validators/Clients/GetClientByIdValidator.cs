551962b2e3f44d889dd74a4faa69866653d5ae10
Hihiz <public@mail.ru>
2026-03-11 15:56:42 +0300

    Добавить валидатор получения деталей клиента

---
+++ WarehouseManagementWeb.Api/Validators/Clients/GetClientByIdValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using WarehouseManagementWeb.Api.Validators.Constants;
+
+namespace WarehouseManagementWeb.Api.Validators.Clients
+{
+    /// <summary>
+    /// Класс валидатора получения деталей клиента.
+    /// </summary>
+    public class GetClientByIdValidator : AbstractValidator<int>
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public GetClientByIdValidator()
+        {
+            RuleFor(x => x)
+                .Must(x => x > 0).WithMessage(ValidationConst.NOT_VALID_CLIENT_ID);
+        }
+    }
+}
