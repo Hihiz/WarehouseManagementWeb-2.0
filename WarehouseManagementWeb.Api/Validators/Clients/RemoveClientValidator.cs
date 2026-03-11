43eccae68e507f2c3ca74717fe02260224c348fa
Hihiz <public@mail.ru>
2026-03-11 15:56:54 +0300

    Добавить валидатор удаления клиента

---
+++ WarehouseManagementWeb.Api/Validators/Clients/RemoveClientValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using WarehouseManagementWeb.Api.Validators.Constants;
+
+namespace WarehouseManagementWeb.Api.Validators.Clients
+{
+    /// <summary>
+    /// Класс валидатора удаления клиента.
+    /// </summary>
+    public class RemoveClientValidator : AbstractValidator<int>
+    {
+        public RemoveClientValidator()
+        {
+            RuleFor(с => с)
+                .Must(с => с > 0).WithMessage(ValidationConst.NOT_VALID_CLIENT_ID);
+        }
+    }
+}
