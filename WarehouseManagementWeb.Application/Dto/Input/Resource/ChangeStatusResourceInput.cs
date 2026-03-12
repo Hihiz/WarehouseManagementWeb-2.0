c037e5d3fba8dc9b8643e43d01077a64a11354d2
Hihiz <public@mail.ru>
2026-03-12 09:36:32 +0300

    Добавить входную модель обновления статуса ресурса

---
+++ WarehouseManagementWeb.Application/Dto/Input/Resource/ChangeStatusResourceInput.cs
@@ -0,0 +1,20 @@
+﻿using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Application.Dto.Input.Resource
+{
+    /// <summary>
+    /// Класс входной модели обновления статуса ресурса.
+    /// </summary>
+    public class ChangeStatusResourceInput
+    {
+        /// <summary>
+        /// Id ресурса.
+        /// </summary>
+        public int ResourceId { get; set; }
+
+        /// <summary>
+        /// Статус ресурса.
+        /// </summary>
+        public DirectoryStatusEnum ResourceStatusEnum { get; set; }
+    }
+}
