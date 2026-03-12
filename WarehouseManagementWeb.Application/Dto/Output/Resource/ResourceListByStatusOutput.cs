55fd0d6dd954e8cee1bba4b516f2fd15a0696bd2
Hihiz <public@mail.ru>
2026-03-12 08:45:35 +0300

    Добавить выходную модель ресурса разделенных по статусам

---
+++ WarehouseManagementWeb.Application/Dto/Output/Resource/ResourceListByStatusOutput.cs
@@ -0,0 +1,20 @@
+﻿using WarehouseManagementWeb.Domain.Entities;
+
+namespace WarehouseManagementWeb.Application.Dto.Output.Resource
+{
+    /// <summary>
+    /// Класс выходной модели ресурсов разделенных по статусам.
+    /// </summary>
+    public class ResourceListByStatusOutput
+    {
+        /// <summary>
+        /// Список активных ресурсов.
+        /// </summary>
+        public IEnumerable<ResourceEntity>? ActiveResources { get; set; }
+
+        /// <summary>
+        /// Список ресурсов находящихся в архиве.
+        /// </summary>
+        public IEnumerable<ResourceEntity>? ArchivedResources { get; set; }
+    }
+}
