fadc702880db8e0146f8df502b763834a284e778
Hihiz <public@mail.ru>
2026-03-12 08:47:18 +0300

    Добавить контроллер ресурсов

---
+++ WarehouseManagementWeb.Api/Controllers/ResourceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManagementWeb.Api.Controllers
+{
+    /// <summary>
+    /// Контроллер ресурсов.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceController : ControllerBase
+    {
+
+    }
+}
