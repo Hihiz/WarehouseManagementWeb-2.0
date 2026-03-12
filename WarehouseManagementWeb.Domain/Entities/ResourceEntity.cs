82397b0e87efc3bb3c6bfe58f8c8e0dab5378e4e
Hihiz <public@mail.ru>
2026-03-12 08:46:23 +0300

    Добавить сущность ресурса

---
+++ WarehouseManagementWeb.Domain/Entities/ResourceEntity.cs
@@ -0,0 +1,25 @@
+﻿using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Domain.Entities
+{
+    /// <summary>
+    /// Класс ресурсов сопоставляется с таблицей directory.resources.
+    /// </summary>
+    public class ResourceEntity
+    {
+        /// <summary>
+        /// PK.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование ресурса.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Статус ресурса в значении перечисления.
+        /// </summary>
+        public DirectoryStatusEnum ResourceStatusEnum { get; set; }
+    }
+}
