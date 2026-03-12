0478b09d197877bcdd29bca2397bc64e1f3207f3
Hihiz <public@mail.ru>
2026-03-12 10:31:14 +0300

    Реализовать метод получения списка ресурсов

--- WarehouseManagementWeb.Application/Services/Resource/ResourceService.cs
+++ WarehouseManagementWeb.Application/Services/Resource/ResourceService.cs
@@ -1,6 +1,9 @@
-﻿using WarehouseManagementWeb.Application.Dto.Input.Resource;
+﻿using Microsoft.Extensions.Logging;
+using WarehouseManagementWeb.Application.Dto.Input.Resource;
 using WarehouseManagementWeb.Application.Dto.Output.Resource;
+using WarehouseManagementWeb.Application.Interfaces.Repositories.Resource;
 using WarehouseManagementWeb.Application.Interfaces.Services.Resource;
+using WarehouseManagementWeb.Domain.Enums;
 
 namespace WarehouseManagementWeb.Application.Services.Resource
 {
@@ -9,14 +12,65 @@ namespace WarehouseManagementWeb.Application.Services.Resource
     /// </summary>
     public class ResourceService : IResourceService
     {
-        public Task ChangeStatusResourceAsync(ChangeStatusResourceInput changeStatusResourceInput)
+        private readonly IResourceRepository _resourceRepository;
+        private readonly ILogger<ResourceService> _logger;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="resourceRepository">Репозиторий ресурсов.</param>
+        /// <param name="logger">Логгер.</param>
+        public ResourceService(IResourceRepository resourceRepository,
+            ILogger<ResourceService> logger)
         {
-            throw new NotImplementedException();
+            _resourceRepository = resourceRepository;
+            _logger = logger;
         }
 
-        public Task CreateResourceAsync(CreateResourceInput createResourceInput)
+        #region Публичные методы.
+
+        /// <inheritdoc />
+        public async Task<ResourceListByStatusOutput> GetResourcesAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                IEnumerable<ResourceOutput> resources = await _resourceRepository.GetResourcesAsync();
+
+                List<ResourceOutput> activeResources = new List<ResourceOutput>(resources.Count(
+                    r => r.ResourceStatusEnum == DirectoryStatusEnum.Active));
+
+                List<ResourceOutput> archivedResources = new List<ResourceOutput>(resources.Count(
+                    r => r.ResourceStatusEnum == DirectoryStatusEnum.Archived));
+
+                foreach (var resource in resources)
+                {
+                    switch (resource.ResourceStatusEnum)
+                    {
+                        case DirectoryStatusEnum.Active:
+                            activeResources.Add(resource);
+                            break;
+
+                        case DirectoryStatusEnum.Archived:
+                            archivedResources.Add(resource);
+                            break;
+                    }
+                }
+
+                ResourceListByStatusOutput result = new ResourceListByStatusOutput()
+                {
+                    ActiveResources = activeResources.OrderByDescending(r => r.Id).ToList(),
+                    ArchivedResources = archivedResources.OrderByDescending(r => r.Id).ToList()
+                };
+
+                return result;
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                throw;
+            }
         }
 
         public Task<IEnumerable<ResourceOutput>> GetActiveResourcesAsync()
@@ -29,7 +83,19 @@ namespace WarehouseManagementWeb.Application.Services.Resource
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<ResourceOutput>> GetResourcesAsync()
+        /// <inheritdoc />
+        public async Task CreateResourceAsync(CreateResourceInput createResourceInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public async Task ChangeStatusResourceAsync(ChangeStatusResourceInput changeStatusResourceInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateResourceAsync(UpdateResourceInput updateResourceInput)
         {
             throw new NotImplementedException();
         }
@@ -39,9 +105,10 @@ namespace WarehouseManagementWeb.Application.Services.Resource
             throw new NotImplementedException();
         }
 
-        public Task UpdateResourceAsync(UpdateResourceInput updateResourceInput)
-        {
-            throw new NotImplementedException();
-        }
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion
     }
 }
