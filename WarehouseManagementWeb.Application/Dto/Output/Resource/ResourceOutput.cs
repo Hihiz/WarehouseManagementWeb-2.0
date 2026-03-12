13318eccb858458d86d84f7e85260846b04f3ba8
Hihiz <public@mail.ru>
2026-03-12 08:45:11 +0300

    Добавить выходную модель ресурса

---
+++ WarehouseManagementWeb.Application/Dto/Output/Resource/ResourceOutput.cs
@@ -0,0 +1,25 @@
+﻿using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Application.Dto.Output.Resource
+{
+    /// <summary>
+    /// Класс выходной модели ресурса.
+    /// </summary>
+    public class ResourceOutput
+    {
+        /// <summary>
+        /// Id ресурса.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование ресурса.
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Статус ресурса в значении перечисления.
+        /// </summary>
+        public DirectoryStatusEnum ResourceStatusEnum { get; set; }
+    }
+}
