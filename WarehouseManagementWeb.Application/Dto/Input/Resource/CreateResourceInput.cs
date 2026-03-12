e3ac788ab92b1a13a112ddc2d092611adb850521
Hihiz <public@mail.ru>
2026-03-12 09:36:45 +0300

    Добавить входную модель создания ресурса

---
+++ WarehouseManagementWeb.Application/Dto/Input/Resource/CreateResourceInput.cs
@@ -0,0 +1,13 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.Resource
+{
+    /// <summary>
+    /// Класс входной модели создания ресурса.
+    /// </summary>
+    public class CreateResourceInput
+    {
+        /// <summary>
+        /// Наименование ресурса.
+        /// </summary>
+        public string? Title { get; set; }
+    }
+}
