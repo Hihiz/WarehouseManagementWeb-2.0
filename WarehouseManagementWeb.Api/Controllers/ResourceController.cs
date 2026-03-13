34072a997f5983e38d135d4827a025f4624edd7b
Hihiz <public@mail.ru>
2026-03-13 08:07:24 +0300

    Добавить эндпоинт получения списка ресурсов

--- WarehouseManagementWeb.Api/Controllers/ResourceController.cs
+++ WarehouseManagementWeb.Api/Controllers/ResourceController.cs
@@ -1,14 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseManagementWeb.Application.Dto.Output.Resource;
+using WarehouseManagementWeb.Application.Interfaces.Services.Resource;
 
 namespace WarehouseManagementWeb.Api.Controllers
 {
     /// <summary>
     /// Контроллер ресурсов.
     /// </summary>
-    [Route("api/[controller]")]
+    [Authorize]
+    [Route("api/directory/resource")]
     [ApiController]
     public class ResourceController : ControllerBase
     {
+        private readonly IResourceService _resourceService;
 
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="resourceService">Сервис ресурсов.</param>
+        public ResourceController(IResourceService resourceService)
+        {
+            _resourceService = resourceService;
+        }
+
+        #region Публичные методы.
+
+        /// <summary>
+        /// Метод получает список ресурсов.
+        /// </summary>
+        /// <returns>Список ресурсов.</returns>
+        [HttpGet]
+        [Route("resources")]
+        public async Task<IActionResult> GetResourcesAsync()
+        {
+            ResourceListByStatusOutput result = await _resourceService.GetResourcesAsync();
+
+            return Ok(result);
+        }
+
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion
     }
 }
