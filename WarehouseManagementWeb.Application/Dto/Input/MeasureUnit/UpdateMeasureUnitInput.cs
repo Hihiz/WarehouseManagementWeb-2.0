d2d4f1dbd47664ffe82bed976c1426416c7397a4
Hihiz <public@mail.ru>
2026-03-13 08:59:55 +0300

    Добавить входную модель редактирования единицы измерения

---
+++ WarehouseManagementWeb.Application/Dto/Input/MeasureUnit/UpdateMeasureUnitInput.cs
@@ -0,0 +1,18 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.MeasureUnit
+{
+    /// <summary>
+    /// Класс входной модели редактирования единицы измерения.
+    /// </summary>
+    public class UpdateMeasureUnitInput
+    {
+        /// <summary>
+        /// Id единицы измерения.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование единицы измерения.
+        /// </summary>
+        public string? Title { get; set; }
+    }
+}
