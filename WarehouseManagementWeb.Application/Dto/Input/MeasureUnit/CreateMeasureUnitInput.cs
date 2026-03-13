01b386e2343cbfc53d2f2a81e16cd3fa1ad0922b
Hihiz <public@mail.ru>
2026-03-13 08:59:41 +0300

    Добавить входную модель создания единицы измерения

---
+++ WarehouseManagementWeb.Application/Dto/Input/MeasureUnit/CreateMeasureUnitInput.cs
@@ -0,0 +1,13 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.MeasureUnit
+{
+    /// <summary>
+    /// Класс входной модели создания единицы измерения.
+    /// </summary>
+    public class CreateMeasureUnitInput
+    {
+        /// <summary>
+        /// Наименование единицы измерения.
+        /// </summary>
+        public string? Title { get; set; }
+    }
+}
