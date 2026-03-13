d42991aa07bab9ae3c1c524210222c9f4b8e5a16
Hihiz <public@mail.ru>
2026-03-13 11:19:30 +0300

    Добавить контроллер единиц измерений

---
+++ WarehouseManagementWeb.Api/Controllers/MeasureUnitController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseManagementWeb.Application.Interfaces.Services.MeasureUnit;
+
+namespace WarehouseManagementWeb.Api.Controllers
+{
+    /// <summary>
+    /// Контроллер единиц измерений.
+    /// </summary>
+    [Route("api/directory/measure-unit")]
+    [ApiController]
+    public class MeasureUnitController : ControllerBase
+    {
+        private readonly IMeasureUnitService _measureUnitService;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="measureUnitService">Сервис единиц измерений.</param>
+        public MeasureUnitController(IMeasureUnitService measureUnitService)
+        {
+            _measureUnitService = measureUnitService;
+        }
+
+        #region Публичные методы.
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion
+    }
+}
