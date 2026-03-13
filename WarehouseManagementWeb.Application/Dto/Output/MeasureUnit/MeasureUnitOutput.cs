58e2bca84d91e8e343d4f3deb1cc016892f7dbd6
Hihiz <public@mail.ru>
2026-03-13 09:00:38 +0300

    Добавить выходную модель единицы измерения

---
+++ WarehouseManagementWeb.Application/Dto/Output/MeasureUnit/MeasureUnitOutput.cs
@@ -0,0 +1,25 @@
+﻿using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Application.Dto.Output.MeasureUnit
+{
+    /// <summary>
+    /// Класс выходной модели единицы измерения.
+    /// </summary>
+    public class MeasureUnitOutput
+    {
+        /// <summary>
+        /// Id единицы измерения.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование единицы измерения.
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Статус единицы измерения в значении перечисления.
+        /// </summary>
+        public DirectoryStatusEnum MeasureUnitStatusEnum { get; set; }
+    }
+}
