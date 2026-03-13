29a6c8e38c47e7df052ca2c8d6f707d2c4a84991
Hihiz <public@mail.ru>
2026-03-13 08:59:10 +0300

    Добавить сущность единицы измерения

---
+++ WarehouseManagementWeb.Domain/Entities/MeasureUnitEntity.cs
@@ -0,0 +1,26 @@
+﻿using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Domain.Entities
+{
+    /// <summary>
+    /// Класс единиц измерения сопоставляется с таблицей directory.measure_units.
+    /// </summary>
+    public class MeasureUnitEntity
+    {
+
+        /// <summary>
+        /// PK.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование единицы измерения.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Статус единицы измерения в значении перечисления.
+        /// </summary>
+        public DirectoryStatusEnum MeasureUnitStatusEnum { get; set; }
+    }
+}
---
+++ WarehouseManagementWeb.Infrastructure/Data/Configurations/MeasureUnitEntityConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WarehouseManagementWeb.Domain.Entities;
+using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Infrastructure.Data.Configurations
+{
+    /// <summary>
+    /// Класс конфигурации сущности единицы измерения.
+    /// </summary>
+    public class MeasureUnitEntityConfiguration : IEntityTypeConfiguration<MeasureUnitEntity>
+    {
+        public void Configure(EntityTypeBuilder<MeasureUnitEntity> entity)
+        {
+            entity.ToTable("measure_units", "directory");
+
+            entity.ToTable(e => e.HasComment("Таблица единиц измерений."));
+
+            entity.Property(e => e.Id)
+                .HasColumnName("id")
+                .HasComment("PK.")
+                .IsRequired();
+
+            entity.Property(e => e.Title)
+                .HasColumnName("title")
+                .HasComment("Наименование единицы измерения.")
+                .IsRequired();
+
+            entity.Property(e => e.MeasureUnitStatusEnum)
+                .HasColumnName("status_enum")
+                .HasColumnType("text")
+                .HasConversion(
+                    v => v.ToString().ToLower(),
+                    v => Enum.Parse<DirectoryStatusEnum>(v, true))
+                .HasDefaultValue(DirectoryStatusEnum.Active)
+                .HasComment("Статус единицы измерения в значении перечисления.")
+                .IsRequired();
+
+            entity.HasIndex(r => r.Title)
+                .IsUnique();
+        }
+    }
+}
