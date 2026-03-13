2d24423789230201dff3709072da809f607913c4
Hihiz <public@mail.ru>
2026-03-13 08:02:21 +0300

    Изменить валидацию клиента

--- WarehouseManagementWeb.Application/Services/Client/ClientService.cs
+++ WarehouseManagementWeb.Application/Services/Client/ClientService.cs
@@ -37,6 +37,8 @@ namespace WarehouseManagementWeb.Application.Services.Client
             {
                 IEnumerable<ClientOutput> clients = await _clientRepository.GetClientsAsync();
 
+                IEnumerable<ClientEntity> rr = new List<ClientEntity>();
+
                 List<ClientOutput> activeClients = new List<ClientOutput>(clients.Count(
                     c => c.ClientStatusEnum == DirectoryStatusEnum.Active));
 
