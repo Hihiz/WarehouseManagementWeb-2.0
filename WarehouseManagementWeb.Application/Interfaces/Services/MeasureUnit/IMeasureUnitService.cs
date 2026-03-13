3088e2f0b463dc182a50f4b86f90653792e08fc0
Hihiz <public@mail.ru>
2026-03-13 10:28:10 +0300

    Добавить интерфейс сервиса единиц измерений

---
+++ WarehouseManagementWeb.Application/Interfaces/Services/MeasureUnit/IMeasureUnitService.cs
@@ -0,0 +1,56 @@
+﻿using WarehouseManagementWeb.Application.Dto.Output.MeasureUnit;
+using WarehouseManagementWeb.Domain.Entities;
+using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Application.Interfaces.Services.MeasureUnit
+{
+    /// <summary>
+    /// Интерфейс сервиса единиц измерений.
+    /// </summary>
+    public interface IMeasureUnitService 
+    {
+        /// <summary>
+        /// Метод получает список единиц измерений.
+        /// </summary>
+        /// <returns>Список единиц измерений.</returns>
+        Task<MeasureUnitListByStatusOutput> GetMeasureUnitsAsync();
+
+        /// <summary>
+        /// Метод получает список активных единиц измерения.
+        /// </summary>
+        /// <returns>Список активных единиц измерения.</returns>
+        Task<IEnumerable<MeasureUnitOutput>> GetActiveMeasureUnitsAsync();
+
+        /// <summary>
+        /// Метод получает единицу измерения по Id.
+        /// </summary>
+        /// <param name="measureUnitId">Id единицы измерения.</param>
+        /// <returns>Данные единицы измерения.</returns>
+        Task<MeasureUnitOutput?> GetMeasureUnitByIdAsync(int measureUnitId);
+
+        /// <summary>
+        /// Метод добавляет единицу измерения.
+        /// </summary>
+        /// <param name="measureUnitEntity">Модель единицы измерения.</param>
+        Task CreateMeasureUnitAsync(MeasureUnitEntity measureUnitEntity);
+
+        /// <summary>
+        /// Метод редактирует единицу измерения.
+        /// </summary>
+        /// <param name="measureUnitEntity">Модель единицы измерения.</param>
+        Task UpdateMeasureUnitAsync(MeasureUnitEntity measureUnitEntity);
+
+        /// <summary>
+        /// Метод обновляет статус единице измерения.
+        /// </summary>
+        /// <param name="measureUnitId">Id единицы измерения.</param>
+        /// <param name="statusEnum">Новый статус единицы измерения.</param>
+        Task ChangeStatusMeasureUnitAsync(int measureUnitId, DirectoryStatusEnum statusEnum);
+
+        /// <summary>
+        /// Метод удаляет единицу измерения.
+        /// </summary>
+        /// <param name="measureUnitId">Id единицы измерения.</param>
+        Task RemoveMeasureUnitAsync(int measureUnitId);
+    }
+}
