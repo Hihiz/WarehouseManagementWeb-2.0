cbf979f21937a6a01bd643ad60594590629e00dd
Hihiz <public@mail.ru>
2026-03-13 08:04:07 +0300

    Добавить валидацию обновления статуса ресурса

---
+++ WarehouseManagementWeb.Api/Validators/Resources/ChangeStatusResourceValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using WarehouseManagementWeb.Api.Validators.Constants;
+using WarehouseManagementWeb.Application.Dto.Input.Resource;
+using WarehouseManagementWeb.Domain.Enums;
+
+namespace WarehouseManagementWeb.Api.Validators.Resources
+{
+    /// <summary>
+    /// Класс валидатора обновления статуса ресурса.
+    /// </summary>
+    public class ChangeStatusResourceValidator : AbstractValidator<ChangeStatusResourceInput>
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public ChangeStatusResourceValidator()
+        {
+            RuleFor(r => r.ResourceId)
+               .Must(r => r > 0).WithMessage(ValidationConst.NOT_VALID_RESOURCE_ID);
+
+            RuleFor(r => r.ResourceStatusEnum)
+                 .IsInEnum()
+                 .NotEqual(DirectoryStatusEnum.Undefined).WithMessage(ValidationConst.NOT_VALID_RESOURCE_STATUS);
+        }
+    }
+}
