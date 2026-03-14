aac222365efb2a0454dc8e009596aa318752fcc7
Hihiz <public@mail.ru>
2026-03-14 20:05:39 +0300

    Добавить сущность документа поступления

---
+++ WarehouseManagementWeb.Domain/Entities/DocumentReceiptEntity.cs
@@ -0,0 +1,38 @@
+﻿namespace WarehouseManagementWeb.Domain.Entities
+{
+    /// <summary>
+    /// Класс документа поступления сопоставляется с таблицей warehouse.document_receipts.
+    /// </summary>
+    public class DocumentReceiptEntity
+    {
+        /// <summary>
+        /// PK.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Номер документа поступления. 
+        /// </summary>
+        public string NumberCode { get; set; }
+
+        /// <summary>
+        /// Дата документа поступления.
+        /// </summary>
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// FK Id клиента документа поступления.
+        /// </summary>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство.
+        /// </summary>
+        public ClientEntity? ClientEntity { get; set; }
+
+        /// <summary>
+        /// Список ресурсов поступлений, которые связанны с документом поступления.
+        /// </summary>
+        public ICollection<ResourceReceiptEntity>? ResourceReceiptEntities { get; set; }
+    }
+}
