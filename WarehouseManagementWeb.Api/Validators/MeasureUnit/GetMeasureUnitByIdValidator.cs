23455a0c37d578bc7139f6311926efe1ae9a34b7
Hihiz <public@mail.ru>
2026-03-14 16:31:48 +0300

    Добавить валидацию обновления статуса единицы измерения

---
+++ WarehouseManagementWeb.Api/Validators/MeasureUnit/GetMeasureUnitByIdValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using WarehouseManagementWeb.Api.Validators.Constants;
+
+namespace WarehouseManagementWeb.Api.Validators.MeasureUnit
+{
+    /// <summary>
+    /// Класс валидатора получения деталей единицы измерения.
+    /// </summary>
+    public class GetMeasureUnitByIdValidator : AbstractValidator<int>
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public GetMeasureUnitByIdValidator()
+        {
+            RuleFor(mu => mu)
+                .Must(mu => mu > 0).WithMessage(ValidationConst.NOT_VALID_MEASURE_UNIT_ID);
+        }
+    }
+}
