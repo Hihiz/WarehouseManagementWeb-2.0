e9357e8353c57119a36b4ba91c03afe517ebff31
Hihiz <public@mail.ru>
2026-03-14 20:06:29 +0300

    Добавить сущность ресурсов поступления

---
+++ WarehouseManagementWeb.Domain/Entities/ResourceReceiptEntity.cs
@@ -0,0 +1,47 @@
+﻿namespace WarehouseManagementWeb.Domain.Entities
+{
+    /// <summary>
+    /// Класс ресурсов поступления сопоставляется с таблицей warehouse.resource_receipts.
+    /// </summary>
+    public class ResourceReceiptEntity
+    {
+        /// <summary>
+        /// PK.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// FK Id документа поступления.
+        /// </summary>
+        public int DocumentReceiptId { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство.
+        /// </summary>
+        public DocumentReceiptEntity? DocumentReceiptEntity { get; set; }
+
+        /// <summary>
+        /// FK Id ресурса.
+        /// </summary>
+        public int? ResourceId { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство.
+        /// </summary>
+        public ResourceEntity? ResourceEntity { get; set; }
+
+        /// FK Id единицы измерения.
+        /// </summary>
+        public int? MeasureUnitId { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство.
+        /// </summary>
+        public MeasureUnitEntity? MeasureUnitEntity { get; set; }
+
+        /// <summary>
+        /// Количество ресурсов поступления.
+        /// </summary>
+        public int? Quantity { get; set; }
+    }
+}
