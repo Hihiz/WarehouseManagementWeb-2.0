af2f9ce05bcfa7052bd1a606fccf8bea60161130
Hihiz <public@mail.ru>
2026-03-14 16:33:27 +0300

    Зарегистрировать сервисы

--- WarehouseManagementWeb.Infrastructure/DependencyInjection.cs
+++ WarehouseManagementWeb.Infrastructure/DependencyInjection.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using WarehouseManagementWeb.Application.Interfaces.Repositories.Client;
+using WarehouseManagementWeb.Application.Interfaces.Repositories.MeasureUnit;
 using WarehouseManagementWeb.Application.Interfaces.Repositories.Resource;
 using WarehouseManagementWeb.Infrastructure.Data;
 using WarehouseManagementWeb.Infrastructure.Identity;
@@ -121,6 +122,7 @@ namespace WarehouseManagementWeb.Infrastructure
         {
             services.AddScoped<IClientRepository, ClientRepository>();
             services.AddScoped<IResourceRepository, ResourceRepository>();
+            services.AddScoped<IMeasureUnitRepository, MeasureUnitRepository>();
         }
     }
 }
