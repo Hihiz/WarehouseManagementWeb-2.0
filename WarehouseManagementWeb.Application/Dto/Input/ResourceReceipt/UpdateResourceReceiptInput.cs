a5373a448586f9ab69b12569b44b5ab60fe72146
Hihiz <public@mail.ru>
2026-03-16 18:42:10 +0300

    Добавить входную модель редактирования ресурса поступления

---
+++ WarehouseManagementWeb.Application/Dto/Input/ResourceReceipt/UpdateResourceReceiptInput.cs
@@ -0,0 +1,33 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.ResourceReceipt
+{
+    /// <summary>
+    /// Класс входной модели редактирования ресурса поступления.
+    /// </summary>
+    public class UpdateResourceReceiptInput
+    {
+        /// <summary>
+        /// Id документа поступления.
+        /// </summary>
+        public int DocumentReceiptId { get; set; }
+
+        /// <summary>
+        /// Номер документа поступления. 
+        /// </summary>
+        public string? DocumentReceiptNumberCode { get; set; }
+
+        /// <summary>
+        /// Дата документа поступления.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Id клиента документа поступления.
+        /// </summary>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Список входящих ресурсов поступлений.
+        /// </summary>
+        public ICollection<ModifyResourceReceiptInput>? ModifyResourceReceiptInputs { get; set; }
+    }
+}
