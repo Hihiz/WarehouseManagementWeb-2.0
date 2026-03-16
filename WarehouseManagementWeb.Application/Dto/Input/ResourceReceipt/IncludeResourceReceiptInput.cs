34ca665f21ae96eff66f64166158a970998ac96a
Hihiz <public@mail.ru>
2026-03-16 18:41:35 +0300

    Добавить входную модель добавления ресурсов поступления

---
+++ WarehouseManagementWeb.Application/Dto/Input/ResourceReceipt/IncludeResourceReceiptInput.cs
@@ -0,0 +1,23 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.ResourceReceipt
+{
+    /// <summary>
+    /// Класс входной модели добавления ресурсов в поступление.
+    /// </summary>
+    public class IncludeResourceReceiptInput
+    {
+        /// <summary>
+        /// Id ресурса.
+        /// </summary>
+        public int ResourceId { get; set; }
+
+        /// <summary>
+        /// Id единицы измерения.
+        /// </summary>
+        public int MeasureUnitId { get; set; }
+
+        /// <summary>
+        /// Количество ресурса.
+        /// </summary>
+        public int ResourceQuantity { get; set; }
+    }
+}
