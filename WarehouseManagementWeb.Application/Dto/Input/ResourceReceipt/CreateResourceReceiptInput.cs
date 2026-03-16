c7c1575a4f4a6e93a1c41a4c7a2926b8cb4417b1
Hihiz <public@mail.ru>
2026-03-16 18:41:01 +0300

    Добавить входную модель создания ресурса поступления

---
+++ WarehouseManagementWeb.Application/Dto/Input/ResourceReceipt/CreateResourceReceiptInput.cs
@@ -0,0 +1,28 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.ResourceReceipt
+{
+    /// <summary>
+    /// Класс входной модели создания ресурса поступления.
+    /// </summary>
+    public class CreateResourceReceiptInput
+    {
+        /// <summary>
+        /// Номер документа поступления. 
+        /// </summary>
+        public string? DocumentReceiptNumberCode { get; set; }
+
+        /// <summary>
+        /// Дата документа поступления.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Id клиента документа поступления.
+        /// </summary>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Список входящих ресурсов поступления.
+        /// </summary>
+        public ICollection<IncludeResourceReceiptInput>? IncludeResourceReceiptInputs { get; set; }
+    }
+}
