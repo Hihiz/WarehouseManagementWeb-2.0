c2dc2fc6f5c0eaaa1ba48b5ceb0bab841f25db79
Hihiz <public@mail.ru>
2026-03-16 18:42:50 +0300

    Добавить выходную модель ресурсов поступления

---
+++ WarehouseManagementWeb.Application/Dto/Output/ResourceReceipt/ResourceReceiptListOutput.cs
@@ -0,0 +1,33 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Output.ResourceReceipt
+{
+    /// <summary>
+    /// Класс выходной модели ресурсов поступлений.
+    /// </summary>
+    public class ResourceReceiptListOutput
+    {
+        /// <summary>
+        /// Id документа поступления.
+        /// </summary>
+        public int DocumentReceiptId { get; set; }
+
+        /// <summary>
+        /// Номер документа поступления.
+        /// </summary>
+        public string? DocumentReceiptNumberCode { get; set; }
+
+        /// <summary>
+        /// Дата документа поступления.
+        /// </summary>
+        public DateTime DocumentReceiptDate { get; set; }
+
+        /// <summary>
+        /// Наименование клиента документа поступения.
+        /// </summary>
+        public string? DocumentClientName { get; set; }
+
+        /// <summary>
+        ///  Список входящих ресурсов.
+        /// </summary>
+        public IEnumerable<ResourceReceiptItemOutput>? Items { get; set; }
+    }
+}
