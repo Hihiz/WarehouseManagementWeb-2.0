30a13ad6ba8b522a49d5ec59b4d2e0edf6df683b
Hihiz <public@mail.ru>
2026-03-17 19:37:24 +0300

    Добавить тест получения документа поступления

---
+++ WarehouseManagementWeb.Tests/Integration/Repository/ResourceReceipt/GetResourceReceiptByDocumentReceiptIdTest.cs
@@ -0,0 +1,13 @@
+﻿namespace WarehouseManagementWeb.Tests.Integration.Repository.ResourceReceipt
+{
+    public class GetResourceReceiptByDocumentReceiptIdTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetResourceReceiptByDocumentReceiptIdAsyncTest()
+        {
+            var result = await resourceReceiptRepository.GetResourceReceiptByDocumentReceiptIdAsync(16);
+
+            Assert.NotNull(result);
+        }
+    }
+}
