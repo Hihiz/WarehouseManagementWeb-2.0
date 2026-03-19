40105a343dfd429e8fc3f4e33977ef47e351ed52
Hihiz <public@mail.ru>
2026-03-19 09:45:01 +0300

    Добавить базовый класс

---
+++ WarehouseManagementWeb.Application/Dto/Input/ResourceReceipt/BaseResourceReceiptInput.cs
@@ -0,0 +1,23 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.ResourceReceipt
+{
+    /// <summary>
+    ///  Класс входной модели ресурсов поступления.
+    /// </summary>
+    public class BaseResourceReceiptInput
+    {
+        /// <summary>
+        /// Id ресурса.
+        /// </summary>
+        public int ResourceId { get; set; }
+
+        /// <summary>
+        /// Id единицы измерения.
+        /// </summary>
+        public int MeasureUnitId { get; set; }
+
+        /// <summary>
+        /// Количество ресурса.
+        /// </summary>
+        public int ResourceQuantity { get; set; }
+    }
+}
