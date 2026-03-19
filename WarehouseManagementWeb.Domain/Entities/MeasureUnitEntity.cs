501394fad133f0470df6ce164ca49727ed3fe81b
Hihiz <public@mail.ru>
2026-03-19 20:16:47 +0300

    Добавить свойство

--- WarehouseManagementWeb.Domain/Entities/MeasureUnitEntity.cs
+++ WarehouseManagementWeb.Domain/Entities/MeasureUnitEntity.cs
@@ -27,5 +27,10 @@ namespace WarehouseManagementWeb.Domain.Entities
         /// Список ресурсов поступлений, которые связанны с единицей измерения.
         /// </summary>
         public ICollection<ResourceReceiptEntity>? ResourceReceiptEntities { get; set; }
+
+        /// <summary>
+        /// Список ресурсов отгрузки, которые связанны с единицей измерения.
+        /// </summary>
+        public ICollection<ResourceShipmentEntity>? ResourceShipmentEntities { get; set; }
     }
 }
