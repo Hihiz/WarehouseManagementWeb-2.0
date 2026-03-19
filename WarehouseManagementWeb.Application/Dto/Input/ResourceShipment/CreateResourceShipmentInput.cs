c60991a8f16657b5ecfd159abaae0df11e83fbac
Hihiz <public@mail.ru>
2026-03-19 20:06:23 +0300

    Добавить входную модель создания отгрузки

---
+++ WarehouseManagementWeb.Application/Dto/Input/ResourceShipment/CreateResourceShipmentInput.cs
@@ -0,0 +1,28 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.ResourceShipment
+{
+    /// <summary>
+    /// Класс входной модели создания ресурса отгрузки.
+    /// </summary>
+    public class CreateResourceShipmentInput
+    {
+        /// <summary>
+        /// Номер документа отгрузки.
+        /// </summary>
+        public string? DocumentShipmenNumberCode { get; set; }
+
+        /// <summary>
+        /// Дата документа отгрузки.
+        /// </summary>
+        public DateTime DocumentShipmentDate { get; set; } 
+
+        /// <summary>
+        ///  Id клиента документа отгрузки.
+        /// </summary>
+        public int DocumentShipmentClientId { get; set; }
+
+        /// <summary>
+        /// Список входящих ресурсов в документ отгрузки.
+        /// </summary>
+        public ICollection<IncludeResourceShipmentInput>? IncludeResourceShipmentInputs { get; set; }
+    }
+}
