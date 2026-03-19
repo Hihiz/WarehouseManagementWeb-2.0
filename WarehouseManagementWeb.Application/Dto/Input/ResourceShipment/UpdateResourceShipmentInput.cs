3af6423e5fd4465950c97222740996957d322296
Hihiz <public@mail.ru>
2026-03-19 20:09:05 +0300

    Добавить входную модель реадктирования ресурсов отгрузки

---
+++ WarehouseManagementWeb.Application/Dto/Input/ResourceShipment/UpdateResourceShipmentInput.cs
@@ -0,0 +1,33 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Input.ResourceShipment
+{
+    /// <summary>
+    /// Класс входной модели редактирования ресурсов документа отгрузки.
+    /// </summary>
+    public class UpdateResourceShipmentInput
+    {       
+        /// <summary>
+        /// Id документа отгрузки.
+        /// </summary>
+        public int DocumentShipmentId { get; set; }
+
+        /// <summary>
+        /// Номер документа отгрузки.
+        /// </summary>
+        public string? DocumentShipmenNumberCode { get; set; }
+
+        /// <summary>
+        /// Дата документа отгрузки.
+        /// </summary>
+        public DateTime DocumentShipmentDate { get; set; }
+
+        /// <summary>
+        ///  Id клиента документа отгрузки.
+        /// </summary>
+        public int DocumentShipmentClientId { get; set; }
+
+        /// <summary>
+        /// Список входящих ресурсов в документ отгрузки.
+        /// </summary>
+        public ICollection<ModifyResourceShipmentInput>? ModifyResourceShipmentInputs { get; set; }
+    }
+}
