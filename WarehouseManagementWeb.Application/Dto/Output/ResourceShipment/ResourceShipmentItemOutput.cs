7651dc180386ea908af6d84a7298367119749bec
Hihiz <public@mail.ru>
2026-03-19 20:10:13 +0300

    Добавить выходную модель документа отгрузки

---
+++ WarehouseManagementWeb.Application/Dto/Output/ResourceShipment/ResourceShipmentItemOutput.cs
@@ -0,0 +1,38 @@
+﻿namespace WarehouseManagementWeb.Application.Dto.Output.ResourceShipment
+{
+    /// <summary>
+    /// Класс выходной модели входящих ресурсов отгрузки.
+    /// </summary>
+    public class ResourceShipmentItemOutput
+    {
+        /// <summary>
+        /// Id ресурса отгрузки.
+        /// </summary>
+        public int ResourceShipmentId { get; set; }
+
+        /// <summary>
+        /// Id ресурса.
+        /// </summary>
+        public int ResourceId { get; set; }
+
+        /// <summary>
+        /// Наименование ресурса.
+        /// </summary>
+        public string? ResourceTitle { get; set; }
+
+        /// <summary>
+        /// Id единицы измерения.
+        /// </summary>
+        public int MeasureUnitId { get; set; }
+
+        /// <summary>
+        /// Наименование единицы измерения.
+        /// </summary>
+        public string? MeasureUnitTitle { get; set; }
+
+        /// <summary>
+        /// Количество ресурса.
+        /// </summary>
+        public int ResourceQuantity { get; set; }
+    }
+}
