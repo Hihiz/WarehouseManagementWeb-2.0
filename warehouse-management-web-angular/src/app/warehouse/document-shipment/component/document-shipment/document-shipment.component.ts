f70e1a60ea240547817e69340f7705f622b57ef1
Hihiz <public@mail.ru>
2026-03-20 19:49:36 +0300

    Добавить компонент документа отгрузки

---
+++ warehouse-management-web-angular/src/app/warehouse/document-shipment/component/document-shipment/document-shipment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-shipment.component',
+  imports: [],
+  templateUrl: './document-shipment.component.html',
+  styleUrl: './document-shipment.component.css',
+})
+export class DocumentShipmentComponent {
+
+}
