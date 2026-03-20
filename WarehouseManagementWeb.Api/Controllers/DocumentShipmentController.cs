eb7d6f719627968f56d5d74e3225824e924934bb
Hihiz <public@mail.ru>
2026-03-20 10:05:29 +0300

    Добавить контроллер документов отгрузок

---
+++ WarehouseManagementWeb.Api/Controllers/DocumentShipmentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseManagementWeb.Application.Interfaces.Services.DocumentShipment;
+
+namespace WarehouseManagementWeb.Api.Controllers
+{
+    /// <summary>
+    /// Класс контроллера документов отгрузок.
+    /// </summary>
+    [Authorize]
+    [Route("api/warehouse/document-shipment")]
+    [ApiController]
+    public class DocumentShipmentController : ControllerBase
+    {
+        private readonly IDocumentShipmentService _documentShipmentService;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="documentShipmentService">Сервис документов отгрузок.</param>
+        public DocumentShipmentController(IDocumentShipmentService documentShipmentService)
+        {
+            _documentShipmentService = documentShipmentService;
+        }
+
+        #region Публичные методы.
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion
+    }
+}
