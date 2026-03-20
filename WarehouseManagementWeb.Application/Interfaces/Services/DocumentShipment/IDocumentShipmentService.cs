45707fcbdb0b49283dcd141d24af404da0fb1f61
Hihiz <public@mail.ru>
2026-03-20 09:45:20 +0300

    Добавить интерфейс сервиса документов отгрузки

---
+++ WarehouseManagementWeb.Application/Interfaces/Services/DocumentShipment/IDocumentShipmentService.cs
@@ -0,0 +1,48 @@
+﻿using WarehouseManagementWeb.Application.Dto.Input.ResourceShipment;
+using WarehouseManagementWeb.Application.Dto.Output.ResourceShipment;
+namespace WarehouseManagementWeb.Application.Interfaces.Services.DocumentShipment
+{
+    /// <summary>
+    /// Интерфейс сервиса документов отгрузок.
+    /// </summary>
+    public interface IDocumentShipmentService
+    {
+        /// <summary>
+        /// Метод получает список ресурсов отгрузки.
+        /// </summary>
+        /// <returns>Список ресурсов отгрузки.</returns>
+        Task<IEnumerable<ResourceShipmentListOutput>> GetResourceShipmentsAsync();
+
+        /// <summary>
+        /// Метод получает ресурс отгрузки по Id документа отгрузки.
+        /// </summary>
+        /// <param name="documentShipmentId">Id документа отгрузки.</param>
+        /// <returns>Данные ресурса отгрузки.</returns>
+        Task<ResourceShipmentListOutput?> GetResourceShipmentByDocumentShipmentIdAsync(int documentShipmentId);
+
+        /// <summary>
+        ///  Метод создает документ отгрузки и добавляет ресурсы отгрузки.
+        /// </summary>
+        /// <param name="input">Входная модель</param>
+        Task CreateResourceShipmentAsync(CreateResourceShipmentInput input);
+
+        /// <summary>
+        /// Метод редактирует ресурсы отгрузки.
+        /// </summary>    
+        /// <param name="input">Входная модель</param>
+        /// ресурсов поступлений.</param>
+        Task UpdateResourceShipmentAsync(UpdateResourceShipmentInput input);
+
+        /// <summary>
+        /// Метод обновляет статус документу отгрузки.
+        /// </summary>
+        /// <param name="input">Входная модель</param>
+        Task ChangeStatusDocumentShipmentAsync(ChangeStatusDocumentShipmentInput input);
+
+        /// <summary>
+        /// Метод удаляет документ отгрузки.
+        /// </summary>
+        /// <param name="documentShipmentId">Id документа отгрузки.</param>
+        Task RemoveDocumentShipmentAsync(int documentShipmentId);
+    }
+}
