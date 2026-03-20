3d0becae5f4671cae6da6cb287711fe0287608bb
Hihiz <public@mail.ru>
2026-03-20 08:47:22 +0300

    Реализовать метод обновления статуса

--- WarehouseManagementWeb.Infrastructure/Repositories/DocumentShipmentRepository.cs
+++ WarehouseManagementWeb.Infrastructure/Repositories/DocumentShipmentRepository.cs
@@ -1,7 +1,9 @@
-﻿using WarehouseManagementWeb.Application.Dto.Output.ResourceShipment;
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseManagementWeb.Application.Dto.Output.ResourceShipment;
 using WarehouseManagementWeb.Application.Interfaces.Repositories.DocumentShipment;
 using WarehouseManagementWeb.Domain.Entities;
 using WarehouseManagementWeb.Domain.Enums;
+using WarehouseManagementWeb.Infrastructure.Data;
 
 namespace WarehouseManagementWeb.Infrastructure.Repositories
 {
@@ -10,22 +12,20 @@ namespace WarehouseManagementWeb.Infrastructure.Repositories
     /// </summary>
     public class DocumentShipmentRepository : IDocumentShipmentRepository
     {
-        public Task ChangeStatusDocumentShipmentAsync(int documentShipmentId, DocumentStatusEnum statusEnum)
+        private readonly ApplicationDbContext _db;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="db">Класс контекста.</param>
+        public DocumentShipmentRepository(ApplicationDbContext db)
         {
-            throw new NotImplementedException();
+            _db = db;
         }
 
-        public Task<bool> CheckDocumentShipmentExistsByIdAndNumberCodeAsync(int documentShipmentId, string documentShipmentNumberCode)
-        {
-            throw new NotImplementedException();
-        }
+        #region Публичные методы.
 
-        public Task<bool> CheckDocumentShipmentExistsByNumberCodeAsync(string documentShipmentNumberCode)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task CreateResourceShipmentAsync(DocumentShipmentEntity documentEntity)
+        public Task<IEnumerable<ResourceShipmentListOutput>> GetResourceShipmentsAsync()
         {
             throw new NotImplementedException();
         }
@@ -35,12 +35,18 @@ namespace WarehouseManagementWeb.Infrastructure.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<ResourceShipmentListOutput>> GetResourceShipmentsAsync()
+        public Task<bool> CheckDocumentShipmentExistsByNumberCodeAsync(string documentShipmentNumberCode)
         {
             throw new NotImplementedException();
         }
 
-        public Task RemoveDocumentShipmentAsync(int documentShipmentId)
+        /// <inheritdoc />
+        public async Task<bool> CheckDocumentShipmentExistsByIdAndNumberCodeAsync(int documentShipmentId, string documentShipmentNumberCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateResourceShipmentAsync(DocumentShipmentEntity documentEntity)
         {
             throw new NotImplementedException();
         }
@@ -49,5 +55,34 @@ namespace WarehouseManagementWeb.Infrastructure.Repositories
         {
             throw new NotImplementedException();
         }
+
+        /// <inheritdoc />
+        public async Task ChangeStatusDocumentShipmentAsync(int documentShipmentId, DocumentStatusEnum statusEnum)
+        {
+            DocumentShipmentEntity? entity = await _db.DocumentShipments
+                .FirstOrDefaultAsync(ds => ds.Id == documentShipmentId);
+
+            if (entity is null)
+            {
+                throw new InvalidOperationException("Ошибка при обновлении статуса документа отгрузки. " +
+                                                   $"DocumentShipmentId: {documentShipmentId}. " +
+                                                   $"Status: {statusEnum}.");
+            }
+
+            entity.DocumentShipmentStatusEnum = statusEnum;
+
+            await _db.SaveChangesAsync();
+        }
+
+        public Task RemoveDocumentShipmentAsync(int documentShipmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion
     }
 }
