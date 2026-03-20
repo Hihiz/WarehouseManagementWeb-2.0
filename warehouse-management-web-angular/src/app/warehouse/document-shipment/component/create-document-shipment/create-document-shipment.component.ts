44382361e39bf9c62a86dab42bcd89d09f8cf083
Hihiz <public@mail.ru>
2026-03-20 19:49:21 +0300

    Добавить компонент создания документа отгрузки

---
+++ warehouse-management-web-angular/src/app/warehouse/document-shipment/component/create-document-shipment/create-document-shipment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-document-shipment.component',
+  imports: [],
+  templateUrl: './create-document-shipment.component.html',
+  styleUrl: './create-document-shipment.component.css',
+})
+export class CreateDocumentShipmentComponent {
+
+}
