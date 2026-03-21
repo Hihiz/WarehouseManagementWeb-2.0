29436822be552a157407b8473eeccfa4000b06f5
Hihiz <public@mail.ru>
2026-03-21 09:02:17 +0300

    Добавить миграции

--- WarehouseManagementWeb.Infrastructure/Migrations/20260321034028_AddTableBalances.Designer.cs
+++ WarehouseManagementWeb.Infrastructure/Migrations/20260321034028_AddTableBalances.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WarehouseManagementWeb.Infrastructure.Data;
@@ -11,9 +12,11 @@ using WarehouseManagementWeb.Infrastructure.Data;
 namespace WarehouseManagementWeb.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260321034028_AddTableBalances")]
+    partial class AddTableBalances
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
