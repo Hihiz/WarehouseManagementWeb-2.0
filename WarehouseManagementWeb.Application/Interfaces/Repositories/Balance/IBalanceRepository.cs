e5db36f495fb9f34f5beb4254775843d3e8e2057
Hihiz <public@mail.ru>
2026-03-21 09:00:37 +0300

    Добавить интерфейс репозитория балансов

---
+++ WarehouseManagementWeb.Application/Interfaces/Repositories/Balance/IBalanceRepository.cs
@@ -0,0 +1,16 @@
+﻿using WarehouseManagementWeb.Application.Dto.Output.Balance;
+
+namespace WarehouseManagementWeb.Application.Interfaces.Repositories.Balance
+{
+    /// <summary>
+    /// Интерфейс репозитория балансов.
+    /// </summary>
+    public interface IBalanceRepository
+    {
+        /// <summary>
+        /// Метод получает список доступных ресурсов баланса.
+        /// </summary>
+        /// <returns>Список доступных ресурсов баланса.</returns>
+        Task<IEnumerable<BalanceOutput>> GetAvailableBalancesAsync();
+    }
+}
