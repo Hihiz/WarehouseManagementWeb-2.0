418783afe7f87e22a3b5cbc030b41010bb9395db
Hihiz <public@mail.ru>
2026-03-21 09:03:36 +0300

    Изменить тест создания документа поступления

--- WarehouseManagementWeb.Tests/Integration/Repository/ResourceReceipt/CreateResourceReceiptTest.cs
+++ WarehouseManagementWeb.Tests/Integration/Repository/ResourceReceipt/CreateResourceReceiptTest.cs
@@ -8,22 +8,23 @@ namespace WarehouseManagementWeb.Tests.Integration.Repository.ResourceReceipt
         public async Task CreateResourceReceiptAsyncTest()
         {
             // Act
-            var client = new ClientEntity { Name = "Test", Address = "Тестовый адрес" };
+            var client = new ClientEntity { Name = "Test" + Guid.NewGuid().ToString(), 
+                Address = "Тестовый адрес" + Guid.NewGuid().ToString() };
             await clientRepository.CreateClientAsync(client);
 
-            var mu1 = new MeasureUnitEntity { Title = "Кирпич М100" };
-            var mu2 = new MeasureUnitEntity { Title = "Цемент М500" };
+            var mu1 = new MeasureUnitEntity { Title = "Кирпич М100" + Guid.NewGuid().ToString() };
+            var mu2 = new MeasureUnitEntity { Title = "Цемент М500" + Guid.NewGuid().ToString() };
             await measureUnitRepository.CreateMeasureUnitAsync(mu1);
             await measureUnitRepository.CreateMeasureUnitAsync(mu2);
 
-            var resource1 = new ResourceEntity { Title = "Кирпич М100" };
-            var resource2 = new ResourceEntity { Title = "Цемент М500" };
+            var resource1 = new ResourceEntity { Title = "Кирпич М100" + Guid.NewGuid().ToString() };
+            var resource2 = new ResourceEntity { Title = "Цемент М500" + Guid.NewGuid().ToString() };
             await resourceRepository.CreateResourceAsync(resource1);
             await resourceRepository.CreateResourceAsync(resource2);
 
             var document = new DocumentReceiptEntity
             {
-                NumberCode = "NumberCode",
+                NumberCode = "NumberCode" + Guid.NewGuid().ToString(),
                 Date = DateTime.UtcNow,
                 ClientId = client.Id,
                 ResourceReceiptEntities = new List<ResourceReceiptEntity>()
