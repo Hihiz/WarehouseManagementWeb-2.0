251cec66ff94088d5cf9a4e579b0f39a801ba3be
Hihiz <public@mail.ru>
2026-03-21 09:00:20 +0300

    Добавить контроллер балансов

---
+++ WarehouseManagementWeb.Api/Controllers/BalanceController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseManagementWeb.Application.Interfaces.Services.Balance;
+
+namespace WarehouseManagementWeb.Api.Controllers
+{
+    /// <summary>
+    /// Класс контроллера балансов.
+    /// </summary>
+    [Authorize]
+    [Route("api/warehouse/balance")]
+    [ApiController]
+    public class BalanceController : ControllerBase
+    {
+        private readonly IBalanceService _balanceService;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="balanceService">Сервис балансов.</param>
+        public BalanceController(IBalanceService balanceService)
+        {
+            _balanceService = balanceService;
+        }
+
+        #region Публичные методы.
+
+        /// <summary>
+        /// Метод получает список доступных ресурсов баланса.
+        /// </summary>
+        /// <returns>Список доступных ресурсов баланса.</returns>
+        [HttpGet]
+        [Route("balance")]
+        public async Task<IActionResult> GetAvailableBalancesAsync() =>
+            Ok(await _balanceService.GetAvailableBalancesAsync());
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion        
+    }
+}
