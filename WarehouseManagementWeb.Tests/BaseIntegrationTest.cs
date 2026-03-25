a02c2405e51b3f98c7e78454bb095cdf37f2f89a
Hihiz <public@mail.ru>
2026-03-25 08:30:35 +0300

    Перенести начальную инициализацию данных

--- WarehouseManagementWeb.Tests/BaseIntegrationTest.cs
+++ WarehouseManagementWeb.Tests/BaseIntegrationTest.cs
@@ -1,5 +1,6 @@
 ﻿using Bogus;
 using Microsoft.EntityFrameworkCore;
+using WarehouseManagementWeb.Domain.Entities;
 using WarehouseManagementWeb.Infrastructure.Data;
 using WarehouseManagementWeb.Infrastructure.Repositories;
 
@@ -50,6 +51,51 @@ namespace WarehouseManagementWeb.Tests
         public async Task DisposeAsync()
         {
             await applicationDbContext.DisposeAsync();
+        }
+
+        protected internal async Task<ClientEntity> SeedClientAsync(string? name = null)
+        {
+            var client = new ClientEntity
+            {
+                Name = name ?? faker.Company.CompanyName(),
+                Address = faker.Address.FullAddress()
+            };
+            await clientRepository.CreateClientAsync(client);
+
+            return client;
+        }
+
+        protected internal async Task<ResourceEntity> SeedResourceAsync(string? title = null)
+        {
+            var resource = new ResourceEntity { Title = title ?? faker.Commerce.ProductName() };
+
+            await resourceRepository.CreateResourceAsync(resource);
+
+            return resource;
+        }
+
+        protected internal async Task<MeasureUnitEntity> SeedMeasureUnitAsync(string? title = null)
+        {
+            var unit = new MeasureUnitEntity { Title = title ?? faker.Commerce.ProductName() };
+            await measureUnitRepository.CreateMeasureUnitAsync(unit);
+
+            return unit;
+        }
+
+        protected internal async Task<BalanceEntity> SeedBalancesAsync(ResourceEntity resource, MeasureUnitEntity unit,
+            int quantity)
+        {
+            var balance = new BalanceEntity
+            {
+                ResourceId = resource.Id,
+                MeasureUnitId = unit.Id,
+                Quantity = quantity
+            };
+
+            await applicationDbContext.Balances.AddAsync(balance);
+            await applicationDbContext.SaveChangesAsync();
+
+            return balance;
         }
     }
 }
