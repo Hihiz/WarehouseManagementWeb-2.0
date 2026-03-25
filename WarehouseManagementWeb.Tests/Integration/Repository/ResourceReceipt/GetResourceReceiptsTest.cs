354841ac4f977f9cae4488f44e274fd5dd0ce4be
Hihiz <public@mail.ru>
2026-03-25 10:19:39 +0300

    Изменить тест получения списка поступления

--- WarehouseManagementWeb.Tests/Integration/Repository/ResourceReceipt/GetResourceReceiptsTest.cs
+++ WarehouseManagementWeb.Tests/Integration/Repository/ResourceReceipt/GetResourceReceiptsTest.cs
@@ -1,4 +1,6 @@
-﻿namespace WarehouseManagementWeb.Tests.Integration.Repository.ResourceReceipt
+﻿using WarehouseManagementWeb.Domain.Entities;
+
+namespace WarehouseManagementWeb.Tests.Integration.Repository.ResourceReceipt
 {
     public class GetResourceReceiptsTest : BaseIntegrationTest
     {
@@ -8,8 +10,36 @@
         [Fact]
         public async Task GetResourceReceiptsAsyncTest()
         {
+            // Arrange
+            var client = await SeedClientAsync();
+
+            var resource1 = await SeedResourceAsync();
+            var resource2 = await SeedResourceAsync();
+
+            var unitKg = await SeedMeasureUnitAsync("кг");
+            var unitPiece = await SeedMeasureUnitAsync("шт");
+
+            var document = new DocumentReceiptEntity
+            {
+                NumberCode = faker.Random.AlphaNumeric(5),
+                ClientId = client.Id,
+                ResourceReceiptEntities = new List<ResourceReceiptEntity>
+                {
+                    new ResourceReceiptEntity
+                    {
+                        ResourceId = resource1.Id,
+                        MeasureUnitId = unitPiece.Id,
+                        Quantity = 100
+                    }
+                }
+            };
+
+            await resourceReceiptRepository.CreateResourceReceiptAsync(document);
+
+            // Act
             var result = await resourceReceiptRepository.GetResourceReceiptsAsync();
 
+            // Assert
             Assert.NotNull(result);
         }
     }
