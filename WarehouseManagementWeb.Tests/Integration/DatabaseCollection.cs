b4a640b59b490ca189f21916d9ce6332bf774436
Hihiz <public@mail.ru>
2026-03-25 22:08:05 +0300

    Изменить базовые классы тестов

---
+++ WarehouseManagementWeb.Tests/Integration/DatabaseCollection.cs
@@ -0,0 +1,10 @@
+﻿namespace WarehouseManagementWeb.Tests.Integration
+{
+    /// <summary>
+    /// Класс коллекции тестов, использующий один общий экземпляр фикстуры.
+    /// </summary>
+    [CollectionDefinition("Database collection")]
+    public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
+    {
+    }
+}
