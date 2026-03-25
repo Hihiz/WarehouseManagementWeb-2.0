c6fcccb997af057094a904e97c0dfd9d1a0f098e
Hihiz <public@mail.ru>
2026-03-25 10:19:01 +0300

    Изменить тест создания поступления

--- WarehouseManagementWeb.Tests/Integration/Repository/ResourceReceipt/CreateResourceReceiptTest.cs
+++ WarehouseManagementWeb.Tests/Integration/Repository/ResourceReceipt/CreateResourceReceiptTest.cs
@@ -1,4 +1,5 @@
-﻿using WarehouseManagementWeb.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseManagementWeb.Domain.Entities;
 
 namespace WarehouseManagementWeb.Tests.Integration.Repository.ResourceReceipt
 {
@@ -10,23 +11,17 @@ namespace WarehouseManagementWeb.Tests.Integration.Repository.ResourceReceipt
         public async Task CreateResourceReceiptAsyncTest()
         {
             // Arrange
-            var client = new ClientEntity { Name = "Test" + Guid.NewGuid().ToString(), 
-                Address = "Тестовый адрес" + Guid.NewGuid().ToString() };
-            await clientRepository.CreateClientAsync(client);
+            var client = await SeedClientAsync();
 
-            var mu1 = new MeasureUnitEntity { Title = "Кирпич М100" + Guid.NewGuid().ToString() };
-            var mu2 = new MeasureUnitEntity { Title = "Цемент М500" + Guid.NewGuid().ToString() };
-            await measureUnitRepository.CreateMeasureUnitAsync(mu1);
-            await measureUnitRepository.CreateMeasureUnitAsync(mu2);
+            var resource1 = await SeedResourceAsync();
+            var resource2 = await SeedResourceAsync();
 
-            var resource1 = new ResourceEntity { Title = "Кирпич М100" + Guid.NewGuid().ToString() };
-            var resource2 = new ResourceEntity { Title = "Цемент М500" + Guid.NewGuid().ToString() };
-            await resourceRepository.CreateResourceAsync(resource1);
-            await resourceRepository.CreateResourceAsync(resource2);
+            var unitKg = await SeedMeasureUnitAsync("кг");
+            var unitPiece = await SeedMeasureUnitAsync("шт");
 
             var document = new DocumentReceiptEntity
             {
-                NumberCode = "NumberCode" + Guid.NewGuid().ToString(),
+                NumberCode = faker.Random.AlphaNumeric(5),
                 Date = DateTime.UtcNow,
                 ClientId = client.Id,
                 ResourceReceiptEntities = new List<ResourceReceiptEntity>()
@@ -35,15 +30,60 @@ namespace WarehouseManagementWeb.Tests.Integration.Repository.ResourceReceipt
             document.ResourceReceiptEntities.Add(new ResourceReceiptEntity
             {
                 ResourceId = resource2.Id,
-                MeasureUnitId = mu2.Id,
+                MeasureUnitId = unitKg.Id,
                 Quantity = 850
             });
             document.ResourceReceiptEntities.Add(new ResourceReceiptEntity
             {
                 ResourceId = resource1.Id,
-                MeasureUnitId = mu1.Id,
+                MeasureUnitId = unitPiece.Id,
                 Quantity = 120
             });
+
+            // Act
+            await resourceReceiptRepository.CreateResourceReceiptAsync(document);
+
+            // Assert 
+            var resourcesIds = new int[] { resource1.Id, resource2.Id };
+            var measureUnitIds = new int[] { unitKg.Id, unitPiece.Id };
+
+            var balances = await applicationDbContext.Balances.Where(b => resourcesIds.Contains(b.ResourceId) &&
+                measureUnitIds.Contains(b.MeasureUnitId)).ToListAsync();
+
+            Assert.Equal(2, balances.Count);
+
+            var balance1 = balances.FirstOrDefault(b => b.ResourceId == resource1.Id &&
+                b.MeasureUnitId == unitPiece.Id);
+            var balance2 = balances.FirstOrDefault(b => b.ResourceId == resource2.Id &&
+                b.MeasureUnitId == unitKg.Id);
+
+            var documentQuantity1 = document.ResourceReceiptEntities
+                .FirstOrDefault(d => d.ResourceId == resource2.Id && 
+                d.MeasureUnitId == unitKg.Id)!.Quantity;
+
+            var documentQuantity2 = document.ResourceReceiptEntities
+                .FirstOrDefault(d => d.ResourceId == resource1.Id && 
+                d.MeasureUnitId == unitPiece.Id)!.Quantity;
+
+            Assert.Equal(120, balance1!.Quantity);
+            Assert.Equal(850, balance2!.Quantity);
+            Assert.Equal(documentQuantity1, balance2.Quantity);
+            Assert.Equal(documentQuantity2, balance1.Quantity);
+        }
+
+        [Fact]
+        public async Task CreateResourceReceiptAsyncResourcesNullTest()
+        {
+            // Arrange
+            var client = await SeedClientAsync();
+
+            var document = new DocumentReceiptEntity
+            {
+                NumberCode = faker.Random.AlphaNumeric(5),
+                Date = DateTime.UtcNow,
+                ClientId = client.Id,
+                ResourceReceiptEntities = new List<ResourceReceiptEntity>()
+            };
 
             // Act & Assert.
             await resourceReceiptRepository.CreateResourceReceiptAsync(document);
