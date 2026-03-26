b2d1428445400b026b47f789172d3ba8fcfd83ba
Hihiz <public@mail.ru>
2026-03-26 10:50:07 +0300

    Добавить базовый класс юнит тестов

---
+++ WarehouseManagementWeb.Tests/Unit/Service/Client/BaseClientServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using WarehouseManagementWeb.Application.Interfaces.Repositories.Client;
+using WarehouseManagementWeb.Application.Services.Client;
+
+namespace WarehouseManagementWeb.Tests.Unit.Service.Client
+{
+    /// <summary>
+    /// Базовый класс юнит тестов сервиса клентов.
+    /// </summary>
+    public class BaseClientServiceTest
+    {
+        protected internal readonly Mock<IClientRepository> mockClientRepository;
+        protected internal readonly Mock<ILogger<ClientService>> mockLogger;
+
+        protected internal readonly ClientService clientService;
+
+        protected internal BaseClientServiceTest()
+        {
+            mockClientRepository = new Mock<IClientRepository>();
+            mockLogger = new Mock<ILogger<ClientService>>();
+
+            clientService = new ClientService(mockClientRepository.Object, mockLogger.Object);
+        }
+    }
+}
