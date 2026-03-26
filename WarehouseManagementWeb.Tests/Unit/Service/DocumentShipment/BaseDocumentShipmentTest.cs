91d9777ba6563cfcc4ffded120d4fd7d7e85bd6d
Hihiz <public@mail.ru>
2026-03-26 11:52:36 +0300

    Добавить базовый класс тестов

---
+++ WarehouseManagementWeb.Tests/Unit/Service/DocumentShipment/BaseDocumentShipmentTest.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.Extensions.Logging;
+using Moq;
+using WarehouseManagementWeb.Application.Interfaces.Repositories.DocumentShipment;
+using WarehouseManagementWeb.Application.Services.DocumentShipment;
+
+namespace WarehouseManagementWeb.Tests.Unit.Service.DocumentShipment
+{
+    /// <summary>
+    /// Базовый класс юнит тестов сервис дкументов отгрузок.
+    /// </summary>
+    public class BaseDocumentShipmentTest
+    {
+        protected internal readonly Mock<IDocumentShipmentRepository> mockDocumentShipmentRepository;
+        protected internal readonly Mock<ILogger<DocumentShipmentService>> mockLogger;
+
+        protected internal readonly DocumentShipmentService documentShipmentService;
+
+        protected internal BaseDocumentShipmentTest()
+        {
+            mockDocumentShipmentRepository = new Mock<IDocumentShipmentRepository>();
+            mockLogger = new Mock<ILogger<DocumentShipmentService>>();
+
+            documentShipmentService = new DocumentShipmentService(mockDocumentShipmentRepository.Object,
+                mockLogger.Object);
+        }
+    }
+}
