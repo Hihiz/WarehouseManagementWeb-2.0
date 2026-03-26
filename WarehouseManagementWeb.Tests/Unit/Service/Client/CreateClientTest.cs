69d0c7c473f8e8776cc521abf44bf8cd915f9a5e
Hihiz <public@mail.ru>
2026-03-26 10:50:53 +0300

    Добавить тест создания клиента

---
+++ WarehouseManagementWeb.Tests/Unit/Service/Client/CreateClientTest.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using WarehouseManagementWeb.Application.Dto.Input.Client;
+using WarehouseManagementWeb.Domain.Entities;
+
+namespace WarehouseManagementWeb.Tests.Unit.Service.Client
+{
+    public class CreateClientTest : BaseClientServiceTest
+    {
+        [Fact]
+        public async Task CreateClientAsyncTest()
+        {
+            // Arrange
+            var input = new CreateClientInput { Name = "Test Client" };
+
+            mockClientRepository
+                .Setup(repo => repo.CheckClientExistsByNameAsync(input.Name))
+                .ReturnsAsync(false);
+
+            //  Act & Assert
+            await clientService.CreateClientAsync(input);
+        }
+
+        [Fact]
+        public async Task CreateClientAsyncNameDuplicateTest()
+        {
+            // Arrange
+            var input = new CreateClientInput { Name = "Test Client" };
+
+            mockClientRepository
+                .Setup(repo => repo.CheckClientExistsByNameAsync(input.Name))
+                .ReturnsAsync(true);
+
+            //  Act & Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(
+                       async () => await clientService.CreateClientAsync(input));
+
+            mockClientRepository.Verify(r => r.CreateClientAsync(It.IsAny<ClientEntity>()),
+                Times.Never);
+        }
+
+        [Fact]
+        public async Task CreateClientAsyncNullTest()
+        {
+            // Arrange
+            CreateClientInput input = null;
+
+            //  Act & Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(
+                     async () => await clientService.CreateClientAsync(input));
+        }
+    }
+}
