1ca9ac0096d4661504c5e322a80845364e4f139b
Hihiz <public@mail.ru>
2026-03-26 10:50:17 +0300

    Добавить тест обновления статуса

---
+++ WarehouseManagementWeb.Tests/Unit/Service/Client/ChangeStatusClientTest.cs
@@ -0,0 +1,32 @@
+﻿using WarehouseManagementWeb.Application.Dto.Input.Client;
+
+namespace WarehouseManagementWeb.Tests.Unit.Service.Client
+{
+    public class ChangeStatusClientTest : BaseClientServiceTest
+    {
+        [Fact]
+        public async Task ChangeStatusClientAsyncTest()
+        {
+            // Arrange
+            var input = new ChangeStatusClientInput
+            {
+                ClientId = 1,
+                ClientStatusEnum = Domain.Enums.DirectoryStatusEnum.Archived
+            };
+
+            //  Act & Assert
+            await clientService.ChangeStatusClientAsync(input);
+        }
+
+        [Fact]
+        public async Task ChangeStatusClientAsyncNullTest()
+        {
+            // Arrange
+            ChangeStatusClientInput input = null;
+
+            //  Act & Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(
+                     async () => await clientService.ChangeStatusClientAsync(input));
+        }
+    }
+}
