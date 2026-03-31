8abec91d41ae1696d420ec10f08dc19518e03386
Hihiz <public@mail.ru>
2026-03-31 22:37:00 +0300

    Убрать юсинг

--- WarehouseManagementWeb.Application/Interfaces/Services/Client/IClientService.cs
+++ WarehouseManagementWeb.Application/Interfaces/Services/Client/IClientService.cs
@@ -1,6 +1,5 @@
 ﻿using WarehouseManagementWeb.Application.Dto.Input.Client;
 using WarehouseManagementWeb.Application.Dto.Output.Client;
-using WarehouseManagementWeb.Domain.Enums;
 
 namespace WarehouseManagementWeb.Application.Interfaces.Services.Client
 {
